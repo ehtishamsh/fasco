1f758302573bf6c0eca7a84a07efb8e57e02c162
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-04-25 06:29:13 +0500

    hero section completed

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,16 +1,45 @@
+import { Button } from "../ui/button";
+
 function Hero() {
   return (
-    <div className="grid grid-cols-3  gap-8 h-[75vh]">
-      <div className=" bg-black rounded-lg">
-        <img src="/hero-right.png" className="h-full w-full" />
+    <div className="grid grid-cols-3  gap-8 h-[78vh]">
+      <div className="rounded-lg overflow-hidden transition-all duration-300">
+        <img
+          src="/hero-right.png"
+          className="h-full w-full rounded-lg transition-all duration-300"
+        />
       </div>
-      <div className="grid grid-cols-1 grid-rows-8 gap-8">
-        <div className="row-span-2 bg-black rounded-lg">Top</div>
-        <div className=" row-span-4 bg-black rounded-lg">middle</div>
-        <div className="row-span-2 bg-black rounded-lg">bottom</div>
+      <div className="grid grid-cols-1 grid-rows-8 gap-4">
+        <div className="row-span-2  rounded-lg">
+          <img
+            src="/top-img.png"
+            className="h-full w-full rounded-lg transition-all duration-300"
+          />
+        </div>
+        <div className=" row-span-4  rounded-lg flex flex-col items-center">
+          <h1 className="text-7xl max-lg:text-6xl max-md:text-5xl max-sm:text-4xl font-semibold transition-all duration-300">
+            ULTIMATE
+          </h1>
+          <h1 className="text-9xl max-lg:text-8xl  max-md:text-7xl max-sm:text-6xl font-bold outline-title transition-all duration-300">
+            SALE
+          </h1>
+          <p className="text-sm  tracking-[2px] mb-5">NEW COLLECTION</p>
+          <Button variant="default" size="lg">
+            SHOP NOW
+          </Button>
+        </div>
+        <div className="row-span-2 bg-black rounded-lg overflow-hidden transition-all duration-300">
+          <img
+            src="/bottom-img.png"
+            className="h-full w-full rounded-lg transition-all duration-300"
+          />
+        </div>
       </div>
-      <div className=" bg-black rounded-lg">
-        <img src="/hero-right.png" className="h-full w-full" />
+      <div className=" rounded-lg overflow-hidden transition-all duration-300">
+        <img
+          src="/hero-left.png"
+          className="h-full w-full rounded-lg transition-all duration-300"
+        />
       </div>
     </div>
   );
