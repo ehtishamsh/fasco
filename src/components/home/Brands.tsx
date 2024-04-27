53a4a9e808f917fa60e46b7fd98175dd79f6a805
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-04-27 06:16:52 +0500

    responsive issues

--- src/components/home/Brands.tsx
+++ src/components/home/Brands.tsx
@@ -1,30 +1,30 @@
 function Brands() {
   return (
-    <div className="py-10 mt-12 max-w-6xl mx-auto">
+    <div className="py-10 mt-12  max-sm:mt-5 max-w-6xl mx-auto">
       <div className="flex items-center justify-between">
         <img
           src="/brand-1.png"
-          className="max-w-44 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
+          className="max-w-40 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
           alt=""
         />
         <img
           src="/brand-2.png"
-          className="max-w-44 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
+          className="max-w-40 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
           alt=""
         />
         <img
           src="/brand-3.png"
-          className="max-w-44 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
+          className="max-w-40 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
           alt=""
         />
         <img
           src="/brand-4.png"
-          className="max-w-44 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
+          className="max-w-40 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
           alt=""
         />
         <img
           src="/brand-5.png"
-          className="max-w-44 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
+          className="max-w-40 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
           alt=""
         />
       </div>
