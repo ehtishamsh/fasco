d97c3b6f04d17537d0822fda96b97015ba16539d
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-04-28 08:24:39 +0500

    shop grid completed

--- src/components/home/ShopGrid.tsx
+++ src/components/home/ShopGrid.tsx
@@ -1,78 +1,76 @@
 import React from "react";
+import { Button } from "../ui/button";
 
 function ShopGrid() {
   return (
-    <div className="">
-      <div className="grid grid-cols-2 max-sm:grid-cols-1">
-        <div className=" text-center">
-          <div className=" grid grid-cols-1 grid-rows-4">
-            <div className="row-span-2 bg-gray-800">
-              <div className="grid grid-cols-2  h-full">
-                <div className="pt-4 pr-10">
-                  <img
-                    src="/ps.png"
-                    alt=""
-                    className="h-full w-full object-cover "
-                  />
-                </div>
-                <div className="p-4 flex justify-center items-start flex-col">
-                  <h1 className="text-5xl font-semibold mb-2 text-white">
-                    Playstation 5
-                  </h1>
-                  <p className="text-sm text-left text-gray-400">
-                    Incredibly powerful CPUs, GPUs, and an SSD with integrated
-                    I/O will redefine your PlayStation experience.
-                  </p>
-                </div>
+    <div className="py-10 h-fit">
+      <div className="grid grid-cols-2 max-md:grid-cols-1 h-fit">
+        <div className=" grid grid-cols-1 grid-rows-2">
+          <div className="bg-gray-800 grid grid-cols-2">
+            <div className="pr-10">
+              <img src="/ps.png" alt="" className="max-w-full " />
+            </div>
+            <div className="p-4 flex justify-center items-start flex-col">
+              <h1 className="text-5xl max-lg:text-4xl max-md:text-3xl max-sm:text-xl font-semibold mb-2 text-white">
+                Playstation 5
+              </h1>
+              <p className="text-sm text-left max-sm:text-xs text-gray-400">
+                Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O
+                will redefine your PlayStation experience.
+              </p>
+            </div>
+          </div>
+
+          <div className=" grid grid-cols-2 bg-gray-300/65">
+            <div className="grid grid-cols-6">
+              <div className=" col-span-2 flex justify-center items-center">
+                <img src="/air.png" alt="" className="max-w-full" />
+              </div>
+              <div className="p-4 flex justify-center items-start flex-col col-span-4">
+                <h1 className="text-3xl max-md:text-xl max-sm:text-base mb-2 text-left">
+                  Apple <br /> AirPods
+                  <span className="font-semibold"> Max</span>
+                </h1>
+                <p className="text-sm text-left text-gray-400 max-sm:text-xs">
+                  Computational audio. Listen, it's powerful.
+                </p>
               </div>
             </div>
-            <div className="row-span-2">
-              <div className=" grid grid-cols-2">
-                <div className="bg-gray-300">
-                  <div className="grid grid-cols-6">
-                    <div className="py-4 col-span-2">
-                      <img
-                        src="/air.png"
-                        alt=""
-                        className="h-full w-full object-cover"
-                      />
-                    </div>
-                    <div className="p-4 flex justify-center items-start flex-col col-span-4">
-                      <h1 className="text-3xl mb-2 text-left">
-                        Apple <br /> AirPods
-                        <span className="font-semibold"> Max</span>
-                      </h1>
-                      <p className="text-sm text-left text-gray-400">
-                        Computational audio. Listen, it's powerful
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="bg-gray-600">
-                  <div className="grid grid-cols-6">
-                    <div className="py-4 col-span-2">
-                      <img
-                        src="/airpods.png"
-                        alt=""
-                        className="h-full w-full object-cover"
-                      />
-                    </div>
-                    <div className="p-4 flex justify-center items-start flex-col col-span-4">
-                      <h1 className="text-3xl mb-2 text-left text-white">
-                        Apple <br /> AirPods
-                      </h1>
-                      <p className="text-sm text-left text-gray-400">
-                        Computational audio. Listen, it's powerful
-                      </p>
-                    </div>
-                  </div>
-                </div>
+
+            <div className="grid grid-cols-6 bg-gray-600/80">
+              <div className=" col-span-2 flex justify-center items-center">
+                <img src="/airpods.png" alt="" className="max-w-full" />
+              </div>
+              <div className="p-4 flex justify-center items-start flex-col col-span-4">
+                <h1 className="text-3xl max-md:text-xl max-sm:text-base mb-2 text-left text-white">
+                  Apple <br /> AirPods
+                </h1>
+                <p className="text-sm text-left text-gray-400 max-sm:text-xs">
+                  Computational audio. Listen, it's powerful & incredibly.
+                </p>
               </div>
             </div>
           </div>
         </div>
-        <div className=" text-center flex justify-center items-center text-7xl">
-          4
+
+        <div className="grid grid-cols-3 bg-gray-900 ">
+          <div className="col-span-2 flex justify-center items-center flex-col ">
+            <h1 className="text-6xl max-lg:text-5xl max-md:text-3xl max-sm:text-2xl mb-2 text-left text-white">
+              MacBook <span className="font-semibold">Air</span>
+            </h1>
+            <p className="text-sm text-left text-gray-400 max-sm:text-xs">
+              The ultimate laptop for the modern age.
+            </p>
+            <Button
+              className="mt-5 text-xl !p-6 max-sm:text-base max-sm:!p-4"
+              variant={"secondary"}
+            >
+              Shop Now
+            </Button>
+          </div>
+          <div className="flex justify-center items-center">
+            <img src="/mb.png" alt="" className=" max-w-full" />
+          </div>
         </div>
       </div>
     </div>
