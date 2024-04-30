d17453880f9a9559f17874dbbadee4a00f63879f
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-04-30 07:00:54 +0500

    added discounted products

--- src/components/home/DiscountedProducts.tsx
+++ src/components/home/DiscountedProducts.tsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
+import ShowcaseProducts from "./ShowcaseProducts";
+
 function DiscountedProducts() {
+  const [products, setProducts] = useState<any[]>([]);
+  useEffect(() => {
+    const fetehData = async () => {
+      try {
+        const getres = await fetch("https://fakestoreapi.com/products", {
+          method: "GET",
+        });
+        const res = await getres.json();
+        const removetenProducts = res.filter((item: any, i: number) => {
+          return i >= 12;
+        });
+        setProducts(removetenProducts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetehData();
+    return () => {
+      setProducts([]);
+    };
+  }, []);
+
+  const handleclick = (e: any) => {
+    e.preventDefault();
+  };
   return (
-    <div className="max-w-6xl  mx-auto mt-">
-      <div className="grid grid-cols-4"></div>
+    <div className="max-w-6xl  mx-auto mt-28 px-2">
+      <Button variant={"secondary"}>
+        <span className={`transition-all duration-300 `}>
+          Discounted Products
+        </span>
+      </Button>
+      <div className="grid grid-cols-4 gap-4 mt-10 max-sm:grid-cols-2 max-md:grid-cols-3">
+        <ShowcaseProducts products={products} handleclick={handleclick} />
+      </div>
     </div>
   );
 }
