3392607f8b7d3de6c8496344849c4e3a09c13751
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-05-02 06:38:02 +0500

    added category to path

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route element={<Layout />} path="/">
             <Route element={<Homepage />} path="/" />
-            <Route element={<Product />} path="/:id" />
+            <Route element={<Product />} path="/:category/:id" />
           </Route>
         </Routes>
       </Router>
