fda38d397b90d94eaa773c7c27b1d2856296bdee
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-05-03 07:16:13 +0500

    details

---
+++ src/components/product/Details.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Details() {
+  return (
+    <div className="max-w-6xl mx-auto px-8 bg-background py-10">
+      <h1 className="text-2xl font-semibold mb-8">Details</h1>
+      <p className="text-gray-400 text-sm tracking-wide mb-8">
+        Just as a book is judged by its cover, the first thing you notice when
+        you pick up a modern smartphone is the display. Nothing surprising,
+        because advanced technologies allow you to practically level the display
+        frames and cutouts for the front camera and speaker, leaving no room for
+        bold design solutions. And how good that in such realities Apple
+        everything is fine with displays. Both critics and mass consumers always
+        praise the quality of the picture provided by the products of the
+        Californian brand. And last year's 6.7-inch Retina panels, which had
+        ProMotion, caused real admiration for many.
+      </p>
+      <h1 className="text-2xl font-semibold mb-8">Screen</h1>
+      <div className="w-full">
+        <div className="flex w-full justify-between items-center text-sm border-b border-gray-300 mb-6 pb-2">
+          <h2>Screen Diagonal</h2>
+          <p>6.7"</p>
+        </div>
+        <div className="flex w-full justify-between items-center text-sm border-b border-gray-300 pb-2">
+          <h2>Screen Resolution</h2>
+          <p>2160x1620</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Details;
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -7,6 +7,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import DeliverySection from "@/components/product/DeliverySection";
 import TopDetailsGrid from "@/components/product/TopDetailsGrid";
+import Details from "@/components/product/Details";
 
 function Product() {
   const path = useParams();
@@ -38,68 +39,73 @@ function Product() {
   console.log(data);
 
   return (
-    <div className="max-w-6xl mx-auto pb-20 px-4">
-      <div className="mb-10">
-        <BreadCrum
-          cat={data?.category}
-          brand={data?.brand}
-          productName={data?.title}
-        />
-      </div>
-      <div className="mt-20">
-        <div className="grid grid-cols-2 gap-8 max-md:gap-6 max-sm:gap-3 max-md:grid-cols-1">
-          <div>
-            <img src={data?.thumbnail} alt="" className="max-w-full" />
-          </div>
-          <div>
-            <h1 className="text-4xl font-bold mb-6 max-md:text-2xl max-sm:text-xl">
-              {data?.title}
-            </h1>
-            <p className="text-3xl mb-6  max-md:text-xl max-sm:text-lg">
-              ${data?.price}
-            </p>
-            {/* Select color */}
-            <div className="flex items-center mb-6">
-              <p className="text-sm mr-4">Select color:</p>
+    <div>
+      <div className="max-w-6xl mx-auto pb-20 px-4">
+        <div className="mb-10">
+          <BreadCrum
+            cat={data?.category}
+            brand={data?.brand}
+            productName={data?.title}
+          />
+        </div>
+        <div className="mt-20">
+          <div className="grid grid-cols-2 gap-8 max-md:gap-6 max-sm:gap-3 max-md:grid-cols-1">
+            <div>
+              <img src={data?.thumbnail} alt="" className="max-w-full" />
+            </div>
+            <div>
+              <h1 className="text-4xl font-bold mb-6 max-md:text-2xl max-sm:text-xl">
+                {data?.title}
+              </h1>
+              <p className="text-3xl mb-6  max-md:text-xl max-sm:text-lg">
+                ${data?.price}
+              </p>
+              {/* Select color */}
+              <div className="flex items-center mb-6">
+                <p className="text-sm mr-4">Select color:</p>
 
-              <ColorCheckbox
-                colors={["bg-red-500", "bg-green-500", "bg-blue-500"]}
-                onChange={setOnChange}
-              />
-            </div>
-            {/* Select variation or size, if any */}
-            <div className="flex items-center justify-between gap-5  max-sm:gap-3">
-              <SelectSize
-                sizes={sizes}
-                selectedSize={selectSize}
-                setSelectedSize={setSelectSize}
-              />
-            </div>
-            <TopDetailsGrid />
-            <p className="text-gray-700/80 mt-6 text-sm leading-relaxed  line-clamp-3">
-              {data?.description}, {data?.description}, {data?.description}
-            </p>
-            <div className=" grid grid-cols-2   max-sm:gap-3 gap-5 mt-6">
-              <Button
-                variant={"outline"}
-                className="border border-foreground py-7"
-                size={"lg"}
-              >
-                Add to Wishlist
-              </Button>
-              <Button
-                variant={"default"}
-                className="border border-foreground py-7"
-                size={"lg"}
-              >
-                Add to Cart
-              </Button>
-            </div>
-            <div className="mt-6 grid grid-cols-3 gap-5">
-              <DeliverySection />
+                <ColorCheckbox
+                  colors={["bg-red-500", "bg-green-500", "bg-blue-500"]}
+                  onChange={setOnChange}
+                />
+              </div>
+              {/* Select variation or size, if any */}
+              <div className="flex items-center justify-between gap-5  max-sm:gap-3">
+                <SelectSize
+                  sizes={sizes}
+                  selectedSize={selectSize}
+                  setSelectedSize={setSelectSize}
+                />
+              </div>
+              <TopDetailsGrid />
+              <p className="text-gray-700/80 mt-6 text-sm leading-relaxed  line-clamp-3">
+                {data?.description}, {data?.description}, {data?.description}
+              </p>
+              <div className=" grid grid-cols-2   max-sm:gap-3 gap-5 mt-6">
+                <Button
+                  variant={"outline"}
+                  className="border border-foreground py-7"
+                  size={"lg"}
+                >
+                  Add to Wishlist
+                </Button>
+                <Button
+                  variant={"default"}
+                  className="border border-foreground py-7"
+                  size={"lg"}
+                >
+                  Add to Cart
+                </Button>
+              </div>
+              <div className="mt-6 grid grid-cols-3 gap-5">
+                <DeliverySection />
+              </div>
             </div>
           </div>
         </div>
+      </div>
+      <div className="mt-20 bg-gray-100 py-20">
+        <Details />
       </div>
     </div>
   );
