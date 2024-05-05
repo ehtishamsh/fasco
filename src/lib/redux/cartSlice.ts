e099aa3375bab1d0e853dd418242782d5d2f5433
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-05-05 05:15:38 +0500

    added remove

--- src/lib/redux/cartSlice.ts
+++ src/lib/redux/cartSlice.ts
@@ -13,18 +13,29 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     add(state, action: { payload: Product }) {
-      // const checkifExist = state.items.filter(
-      //   (item) => item.id === action.payload.id
-      // );
-      // if (!checkifExist) {
-      state.items.push({ ...action.payload, quantity: 1 });
-      // } else
-      //   state.items.map((item) => {
-      //     return { ...item, quantity: item.quantity && item.quantity + 1 };
-      //   });
+      const checkifExist = state.items.findIndex((item) => {
+        return item.id === action.payload.id;
+      });
+      if (checkifExist === -1) {
+        state.items.push({
+          ...action.payload,
+          quantity: 1,
+        });
+      } else if (checkifExist !== -1) {
+        state.items[checkifExist].quantity =
+          (state.items[checkifExist].quantity || 0) + 1;
+      }
+    },
+    remove(state, action: { payload: Product }) {
+      const checkifExist = state.items.findIndex((item) => {
+        return item.id === action.payload.id;
+      });
+      if (checkifExist !== -1) {
+        state.items.splice(checkifExist, 1);
+      }
     },
   },
 });
 
 export default cartSlice.reducer;
-export const { add } = cartSlice.actions;
+export const { add, remove } = cartSlice.actions;
