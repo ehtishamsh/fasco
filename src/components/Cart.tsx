299307c4d67478e72aef12a747b15fce7ff8355e
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-05-06 07:36:38 +0500

    cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BiX } from "react-icons/bi";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
@@ -19,11 +19,19 @@ function Cart({
   const cartItems = useSelector<CartState, Product[]>(
     (state) => state?.cart?.items
   );
-  const [data, setData] = React.useState<Product[]>(cartItems);
+  const [data, setData] = useState<Product[]>(cartItems);
   const dispatch = useDispatch();
   const { toast } = useToast();
+  const [total, setTotal] = useState(0);
   useEffect(() => {
     setData(cartItems);
+    setTotal(() => {
+      let sum = 0;
+      cartItems.forEach((item) => {
+        sum += Number(item.price) * (item?.quantity || 1);
+      });
+      return sum;
+    });
   }, [data, cartItems]);
   function handleRemove(item: Product) {
     dispatch(remove(item));
@@ -36,21 +44,21 @@ function Cart({
   const mapData = data?.map((item, i) => {
     return (
       <div key={i}>
-        <div className="grid grid-cols-4 gap-4 p-2 rounded-md  transition-all duration-300">
+        <div className="grid grid-cols-4 gap-4 p-2 rounded-md mb-5 bg-gray-100  transition-all duration-300">
           <div className="col-span-1 flex justify-center items-center">
             <img src={item.thumbnail} alt="" className="max-h-[120px]" />
           </div>
           <div className="col-span-3  flex justify-between">
             <div>
+              <p className="text-sm text-gray-500">{item.brand}</p>
               <Link
                 to={`${item.category}/${item.id}`}
-                className="text-xl font-semibold underline"
+                className="text-base font-semibold line-clamp-1"
               >
-                {item.title}
+                {item.quantity || 1} x {item.title}
               </Link>
-              <p className="text-sm text-gray-500 mt-3">
-                {item.quantity || 1} x
-                <span className="font-semibold text-black">${item.price}</span>
+              <p className="text-sm text-yellow-600 font-semibold mt-3">
+                ${item.price}
               </p>
             </div>
             <div className="">
@@ -58,8 +66,9 @@ function Cart({
                 variant={"ghost"}
                 size={"icon"}
                 onClick={() => handleRemove(item)}
+                className="rounded-full bg-yellow-300 hover:bg-yellow-400"
               >
-                <BiX className="text-3xl text-gray-500" />
+                <BiX className="text-2xl text-yellow-800" />
               </Button>
             </div>
           </div>
@@ -82,25 +91,57 @@ function Cart({
       transition={{ duration: 0.3 }}
       className="absolute w-full backdrop-brightness-50 overflow-hidden backdrop-blur-sm h-svh  top-0 right-0 left-0 bottom-0 z-50"
     >
-      <div className="w-full h-full flex justify-end">
+      <div className="w-full h-full flex justify-end ">
         <motion.div
           animate={{ x: openCart ? "0%" : "100%" }}
           transition={{ duration: 0.3, type: "tween" }}
-          className="max-w-[30%] w-full bg-background h-full"
+          className="max-w-[30%] max-lg:max-w-[50%] max:md:max-w-[60%] max-sm:max-w-[100%] w-full bg-background h-full bg-gray-200"
         >
-          <div className="p-4 flex justify-center items-center relative w-full">
+          <div className="p-4 flex justify-center items-center relative w-full bg-white">
+            <h1 className="text-3xl font-normal">YOUR BAG</h1>
             <Button
               variant={"outline"}
               onClick={() => setOpenCart(!openCart)}
               size={"icon"}
-              className="absolute top-4 left-5"
+              className="absolute top-4 right-5"
             >
               <BiX className="text-4xl" />
             </Button>
-            <h1 className="text-3xl font-normal">YOUR BAG</h1>
           </div>
           <Separator />
-          <div className="mt-8 px-5 py-2">{mapData}</div>
+          {data && data.length > 0 ? (
+            <>
+              <div className="mt-8 px-5 py-2 max-h-[59vh] overflow-y-auto">
+                {mapData}
+              </div>
+              <div className="absolute bottom-0  left-0 right-0 px-5 py-5 bg-white">
+                <div className="flex justify-between items-center">
+                  <h1 className="text-base font-semibold">Subtotal</h1>{" "}
+                  <h1 className="text-base font-semibold">${total}</h1>
+                </div>
+                <Separator />
+                <div className="flex justify-center items-center flex-col gap-4 mt-4">
+                  <Link to="/cart" className="w-full">
+                    <Button className="w-full" variant={"outline"} size={"lg"}>
+                      View Cart
+                    </Button>
+                  </Link>
+                  <Link to="/checkout" className="w-full">
+                    <Button className="w-full" size={"lg"}>
+                      Checkout
+                    </Button>
+                  </Link>
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="flex flex-col justify-center items-center h-full px-10">
+              <img src="/cart_empty.png" alt="" />
+              <p className="text-center text-gray-500 font-medium mt-5">
+                No Items in cart
+              </p>
+            </div>
+          )}
         </motion.div>
       </div>
     </motion.div>
--- src/lib/redux/types.ts
+++ src/lib/redux/types.ts
@@ -4,6 +4,8 @@ export interface Product {
   price: string;
   thumbnail: string;
   category: string;
+  description: string;
+  brand?: string;
   quantity?: number; // Add quantity property for cart items
 }
 export type CartState = {
