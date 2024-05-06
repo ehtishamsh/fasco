663ae7955c11726cf9c78b47eeadd19f4c491212
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-05-06 08:49:13 +0500

    some changes

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,5 +1,11 @@
+import MainCart from "@/components/ShoppingCart/MainCart";
+
 function ShoppingCart() {
-  return <div className="max-w-6xl mx-auto px-4">ShoppingCart</div>;
+  return (
+    <div className="max-w-6xl mx-auto px-4">
+      <MainCart />
+    </div>
+  );
 }
 
 export default ShoppingCart;
