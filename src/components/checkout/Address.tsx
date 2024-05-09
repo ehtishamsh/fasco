e77bde75f7a1e916263cdfe7fe4a97e08ee9207c
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-05-09 08:22:13 +0500

    responsive

--- src/components/checkout/Address.tsx
+++ src/components/checkout/Address.tsx
@@ -1,6 +1,5 @@
 import { BiEdit, BiPlus, BiX } from "react-icons/bi";
 import { Button } from "../ui/button";
-import { Checkbox } from "../ui/checkbox";
 import { StepData } from "@/lib/redux/types";
 import ButtonNextBack from "./ButtonNextBack";
 
@@ -32,61 +31,73 @@ function Address({
 
   return (
     <div>
-      <h1 className="text-xl font-semibold">Select Address</h1>
-      <div className="grid grid-cols-1 gap-9 mt-10">
-        <div className="flex gap-4 bg-gray-200/70 p-6 rounded-lg">
+      <h1 className="text-xl font-semibold max-sm:text-base">Select Address</h1>
+      <div className="grid grid-cols-1 gap-9 max-sm:gap-4 mt-10 max-sm:mt-5">
+        <div className="flex gap-4 bg-gray-100/80 p-6 max-sm:p-4 max-sm:gap-2 rounded-lg">
           <div className="flex justify-center">
-            <Checkbox className="rounded-full mt-[5px] " />
+            <input type="checkbox" className="custom-checkbox mt-[5px] " />
           </div>
           <div className="w-full">
-            <div className="flex items-center gap-3 mb-4">
-              <h1 className="">2118 Thornridge</h1>
-              <span className="bg-foreground text-sm text-white px-[8px] py-[4px] rounded-lg">
+            <div className="flex items-center gap-3 mb-4 max-sm:gap-2 max-sm:mb-2">
+              <h1 className="max-sm:text-xs">2118 Thornridge</h1>
+              <span className="bg-foreground text-sm max-sm:text-xs text-white px-[8px] py-[4px] rounded-lg">
                 Home
               </span>
             </div>
-            <h1 className="mb-2">
+            <h1 className="mb-2 max-sm:text-xs">
               2118 Thornridge Cir. Syracuse, Connecticut 35624
             </h1>
 
-            <span>(209) 555-0104</span>
+            <span className="max-sm:text-xs">(209) 555-0104</span>
           </div>
           <div className="flex justify-center items-center">
-            <Button size={"icon"}>
-              <BiEdit size={20} />
+            <Button
+              size={"icon"}
+              className="max-sm:py-1 max-sm:!px-1 max-sm:w-fit max-sm:h-fit"
+            >
+              <BiEdit className="text-xl max-sm:text-base" />
             </Button>
           </div>
           <div className="flex justify-center items-center">
-            <Button size={"icon"}>
-              <BiX size={20} />
+            <Button
+              size={"icon"}
+              className="max-sm:py-1 max-sm:!px-1 max-sm:w-fit max-sm:h-fit"
+            >
+              <BiX className="text-xl max-sm:text-base" />
             </Button>
           </div>
         </div>
-        <div className="flex gap-4 bg-gray-200/70 p-6 rounded-lg">
+        <div className="flex gap-4 bg-gray-100/80 p-6 max-sm:p-4 max-sm:gap-2 rounded-lg">
           <div className="flex justify-center">
-            <Checkbox className="rounded-full mt-[5px]" />
+            <input type="checkbox" className="custom-checkbox mt-[5px] " />
           </div>
           <div className="w-full">
-            <div className="flex items-center gap-3 mb-4">
-              <h1 className="">2118 Thornridge</h1>
-              <span className="bg-foreground text-sm text-white px-[8px] py-[4px] rounded-lg">
+            <div className="flex items-center gap-3 mb-4 max-sm:gap-2 max-sm:mb-2">
+              <h1 className="max-sm:text-xs">2118 Thornridge</h1>
+              <span className="bg-foreground text-sm max-sm:text-xs text-white px-[8px] py-[4px] rounded-lg">
                 Home
               </span>
             </div>
-            <h1 className="mb-2">
+            <h1 className="mb-2 max-sm:text-xs">
               2118 Thornridge Cir. Syracuse, Connecticut 35624
             </h1>
 
-            <span>(209) 555-0104</span>
+            <span className="max-sm:text-xs">(209) 555-0104</span>
           </div>
           <div className="flex justify-center items-center">
-            <Button size={"icon"}>
-              <BiEdit size={20} />
+            <Button
+              size={"icon"}
+              className="max-sm:py-1 max-sm:!px-1 max-sm:w-fit max-sm:h-fit"
+            >
+              <BiEdit className="text-xl max-sm:text-base" />
             </Button>
           </div>
           <div className="flex justify-center items-center">
-            <Button size={"icon"}>
-              <BiX size={20} />
+            <Button
+              size={"icon"}
+              className="max-sm:py-1 max-sm:!px-1 max-sm:w-fit max-sm:h-fit"
+            >
+              <BiX className="text-xl max-sm:text-base" />
             </Button>
           </div>
         </div>
