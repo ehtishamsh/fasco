11f0a03a165e2fa4fd8e8d7a6738b32c725d6acb
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-05-12 05:56:52 +0500

    color

---
+++ src/components/filter/Color.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+function Color({
+  color,
+  setColor,
+}: {
+  color: string;
+  setColor: React.Dispatch<React.SetStateAction<string[]>>;
+}) {
+  const [selectedColor, setSelectedColor] = useState(color[0]);
+  const handleClick = (color: any) => {
+    setSelectedColor(color);
+    setColor((prev) => {
+      const filter = prev.filter((item) => item !== color);
+      if (prev.includes(color)) {
+        return filter;
+      } else {
+        return [...filter, color];
+      }
+    });
+  };
+  console.log(color, selectedColor);
+  return (
+    <>
+      <button
+        key={color}
+        type="button"
+        className={`w-6 h-6 rounded-full focus:outline-none transition-all duration-200 border-2 border-foreground `}
+        style={{
+          backgroundColor: color,
+          opacity: color === selectedColor ? 1 : 0.5,
+        }}
+        onClick={() => handleClick(color)}
+      />
+    </>
+  );
+}
+
+export default Color;
