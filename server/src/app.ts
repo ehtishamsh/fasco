6f3291dbd3c9c6303a431277782b0917f75ca5bb
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-05-23 23:01:37 +0500

    added comments

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,14 +2,19 @@ import express from "express";
 import userRoutes from "./routes/userRoutes";
 import errorHandler from "./middleware/errorHandler";
 
+// Express
 const app = express();
+
+// Cors
 const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 
+// Routes
 app.use("/apiv1", userRoutes);
 
+// ERROR HANDLER
 app.use(errorHandler);
 
 export default app;
