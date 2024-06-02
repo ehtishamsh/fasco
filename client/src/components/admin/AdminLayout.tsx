035ad639a4ccb4dc3eaaf6d1458975338d9d6cc3
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-06-02 05:12:31 +0500

    added navbar

--- client/src/components/admin/AdminLayout.tsx
+++ client/src/components/admin/AdminLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import AdminNavbar from "./AdminNavbar";
 
 function AdminLayout() {
   return (
     <>
+      <AdminNavbar />
       <main className="flex overflow-hidden">
         <Sidebar />
         <div className="w-full pt-16">
---
+++ client/src/components/admin/AdminNavbar.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
+import UserNav from "./UserNav";
+
+function AdminNavbar() {
+  return (
+    <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
+      <nav className="h-16 flex items-center justify-between px-4">
+        <div className="flex gap-2 justify-center items-center">
+          <MobileNav />
+          <Link to={"/admin"}>THE BLOG</Link>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <UserNav />
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default AdminNavbar;
