335fd304e0f7b3e6cab978a86b2d9bfe8aa77f00
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-06-05 02:24:40 +0500

    signin and sign up

---
+++ client/src/components/signin/Signin.tsx
@@ -0,0 +1,140 @@
+import Lottie from "lottie-react";
+import BG from "../animation/bg.json";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Reveal } from "../animation/Reveal";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { toast } from "../ui/use-toast";
+const FormSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Email address is required" })
+    .email({ message: "Please enter a valid email address" }),
+  password: z
+    .string()
+    .min(1, { message: "Password is required" })
+    .min(6, { message: "Password must be at least 6 characters" }),
+});
+function Signin() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    try {
+      const response = await fetch("http://localhost:4000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: data.email,
+          password: data.password,
+        }),
+      });
+      const user = await response.json();
+
+      if (response.ok) {
+        localStorage.setItem("token", user.token);
+        localStorage.setItem("user", JSON.stringify(user.user));
+        toast({
+          title: "Success",
+          description: "User logged in successfully",
+          variant: "success",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Error",
+        description: "An error occurred while creating the user",
+        variant: "destructive",
+      });
+    }
+  }
+
+  return (
+    <Reveal width="100%">
+      <div className=" overflow-hidden  mb-20 max-sm:mb-10 bg-center">
+        <div className="max-w-6xl mx-auto">
+          <div className="w-full h-full grid grid-cols-2 max-md:grid-cols-1">
+            <div className="overflow-hidden rounded-md  border border-border">
+              <Lottie
+                animationData={BG}
+                className=" h-full  w-full"
+                loop
+                autoplay
+              />
+            </div>
+            <div className="border border-border ">
+              <div className="flex  flex-col items-center justify-center h-full p-8 max-md:p-6 max-sm:p-4">
+                <img src="/logo.png" className="max-w-[100px]" alt="" />
+                <h1 className="text-2xl mt-6 max-sm:mt-4">Sign up</h1>
+                <p className="text-sm mt-2 text-gray-400 mb-4">
+                  Create your account to get started.
+                </p>
+                <Form {...form}>
+                  <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="w-full px-28 max-sm:px-10 max-md:px-16 space-y-2"
+                  >
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Enter your email" {...field} />
+                          </FormControl>
+                          <FormDescription></FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Password</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="Enter your password"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormDescription></FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <Button type="submit">Submit</Button>
+                  </form>
+                </Form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Reveal>
+  );
+}
+
+export default Signin;
---
+++ client/src/components/signup/Signup.tsx
@@ -0,0 +1,193 @@
+import Lottie from "lottie-react";
+import BG from "../animation/bg.json";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Reveal } from "../animation/Reveal";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { toast } from "../ui/use-toast";
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z
+    .string()
+    .min(1, { message: "Email address is required" })
+    .email({ message: "Please enter a valid email address" }),
+  password: z
+    .string()
+    .min(1, { message: "Password is required" })
+    .min(6, { message: "Password must be at least 6 characters" }),
+  confirmPassword: z
+    .string()
+    .min(1, { message: "Confirm password is required" })
+    .min(6, { message: "Password must be at least 6 characters" }),
+});
+function Signup() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    if (data.password !== data.confirmPassword) {
+      toast({
+        title: "Error",
+        description: "Passwords do not match",
+        variant: "destructive",
+      });
+      return;
+    }
+    try {
+      const response = await fetch("http://localhost:4000/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: data.username,
+          email: data.email,
+          password: data.password,
+        }),
+      });
+
+      if (response.ok) {
+        toast({
+          title: "Success",
+          description: "User created successfully",
+          variant: "success",
+        });
+        setTimeout(() => {
+          window.location.href = "/login";
+        }, 2000);
+      }
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Error",
+        description: "An error occurred while creating the user",
+        variant: "destructive",
+      });
+    }
+  }
+
+  return (
+    <Reveal width="100%">
+      <div className=" overflow-hidden  mb-20 max-sm:mb-10 bg-center">
+        <div className="max-w-6xl mx-auto">
+          <div className="w-full h-full grid grid-cols-2 max-md:grid-cols-1">
+            <div className="overflow-hidden rounded-md  border border-border">
+              <Lottie
+                animationData={BG}
+                className=" h-full  w-full"
+                loop
+                autoplay
+              />
+            </div>
+            <div className="border border-border ">
+              <div className="flex  flex-col items-center justify-center h-full p-8 max-md:p-6 max-sm:p-4">
+                <img src="/logo.png" className="max-w-[100px]" alt="" />
+                <h1 className="text-2xl mt-6 max-sm:mt-4">Sign up</h1>
+                <p className="text-sm mt-2 text-gray-400 mb-4">
+                  Create your account to get started.
+                </p>
+                <Form {...form}>
+                  <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="w-full px-28 max-sm:px-10 max-md:px-16 space-y-2"
+                  >
+                    <FormField
+                      control={form.control}
+                      name="username"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Username</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="Enter your username"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormDescription>
+                            This is your public display name.
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Enter your email" {...field} />
+                          </FormControl>
+                          <FormDescription></FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Password</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="Enter your password"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormDescription></FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="confirmPassword"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Confirm Password</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="Enter your password"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormDescription></FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <Button type="submit">Submit</Button>
+                  </form>
+                </Form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Reveal>
+  );
+}
+
+export default Signup;
