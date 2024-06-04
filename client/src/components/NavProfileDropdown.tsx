67d3ceb4a9ff957a7e855733d806459a3bbf0c58
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-06-05 04:17:54 +0500

    sign out functionality and check auth

--- client/src/components/NavProfileDropdown.tsx
+++ client/src/components/NavProfileDropdown.tsx
@@ -12,8 +12,18 @@ import {
 import { BiLogIn } from "react-icons/bi";
 import { Button } from "./ui/button";
 import { BsPerson } from "react-icons/bs";
+import { UserCircle } from "lucide-react";
+import { DashboardIcon } from "@radix-ui/react-icons";
+import { GoSignOut } from "react-icons/go";
 
 export function NavProfileDropdown() {
+  const checkLogin = localStorage.getItem("token");
+  const checkUser = JSON.parse(localStorage.getItem("user") || "{}");
+  const handleSignOut = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    window.location.href = "/";
+  };
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -29,25 +39,63 @@ export function NavProfileDropdown() {
         <DropdownMenuLabel>Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem
-            className=" cursor-pointer"
-            onClick={() => (window.location.href = "/signin")}
-          >
-            Sign in
-            <DropdownMenuShortcut>
-              <BiLogIn size={20} />
-            </DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem
-            className=" cursor-pointer"
-            onClick={() => (window.location.href = "/signup")}
-          >
-            Sign up
-            <DropdownMenuShortcut>
-              <BiLogIn size={20} />
-            </DropdownMenuShortcut>
-          </DropdownMenuItem>
+          {!checkLogin ? (
+            <>
+              <DropdownMenuItem
+                className=" cursor-pointer"
+                onClick={() => (window.location.href = "/signin")}
+              >
+                Sign in
+                <DropdownMenuShortcut>
+                  <BiLogIn size={20} />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className=" cursor-pointer"
+                onClick={() => (window.location.href = "/signup")}
+              >
+                Sign up
+                <DropdownMenuShortcut>
+                  <BiLogIn size={20} />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </>
+          ) : (
+            <>
+              {checkUser?.role && (
+                <DropdownMenuItem
+                  className=" cursor-pointer"
+                  onClick={() => (window.location.href = "/admin")}
+                >
+                  Admin
+                  <DropdownMenuShortcut>
+                    <UserCircle />
+                  </DropdownMenuShortcut>
+                </DropdownMenuItem>
+              )}
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className=" cursor-pointer"
+                onClick={() => (window.location.href = "/dashboard")}
+              >
+                Dashboard
+                <DropdownMenuShortcut>
+                  <DashboardIcon />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className=" cursor-pointer"
+                onClick={handleSignOut}
+              >
+                Sign out
+                <DropdownMenuShortcut>
+                  <GoSignOut />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </>
+          )}
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
