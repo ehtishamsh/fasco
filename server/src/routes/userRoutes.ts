57daceaf0b174158eb5ad3d3c62e04e3e2a1db0f
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-06-05 02:26:57 +0500

    routes

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { register } from "../controllers/RegisterController";
+import { login } from "../controllers/loginController";
 const router = Router();
 
+router.post("/api/login", login);
 router.post("/api/register", register);
 
 export default router;
