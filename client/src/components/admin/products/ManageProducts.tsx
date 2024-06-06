772b42f047e306bf2361ad72251ef3a433dfcbb7
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-06-06 23:13:43 +0500

    getting products and showing them

--- client/src/components/admin/products/ManageProducts.tsx
+++ client/src/components/admin/products/ManageProducts.tsx
@@ -1,5 +1,42 @@
+import { BreadCrum } from "@/components/BreadCrum";
+import { DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
+import ProductsTable from "./ProductsTable";
+import { Link } from "react-router-dom";
+import { Plus } from "lucide-react";
+
 function ManageProducts() {
-  return <div className="">ManageProducts</div>;
+  const session = JSON.parse(localStorage.getItem("user") || "{}");
+  const token = localStorage.getItem("token");
+  if (session.role !== "admin") {
+    window.location.href = "/";
+  }
+  if (!token) {
+    window.location.href = "/signin";
+  }
+  return (
+    <div className="px-2 max-w-6xl">
+      <div className="mt-8 px-5 max-sm:px-0 flex flex-col gap-5 w-full">
+        <BreadCrum cat="admin" brand="products" />
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-3xl font-bold tracking-tight">User Blogs</h1>
+            <span className="text-sm text-muted-foreground">
+              Manage the user blogs.
+            </span>
+          </div>
+          <Link
+            to={"/admin/products/new"}
+            className="flex items-center gap-5 bg-foreground max-sm:text-sm text-white border-placeholder-default border px-3 py-2 rounded-lg"
+          >
+            <Plus className="w-5 h-5 max-sm:w-4 max-sm:h-4" /> Add
+          </Link>
+        </div>
+        <DropdownMenuSeparator />
+        <ProductsTable />
+        <DropdownMenuSeparator />
+      </div>
+    </div>
+  );
 }
 
 export default ManageProducts;
