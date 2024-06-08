93947bbf512e20f94a93ce707d74597f3bbb62b8
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-06-08 23:26:26 +0500

    cate

---
+++ client/src/components/admin/category/CateTable.tsx
@@ -0,0 +1,29 @@
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
+import { useEffect, useState } from "react";
+
+export interface Cate {
+  id: string;
+  name: string;
+}
+export default function CatesTable() {
+  const [getData, setData] = useState<Cate[]>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const req = await fetch("http://localhost:4000/api/categories");
+        const res = await req.json();
+        setData(res.categories);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className=" mx-auto pb-8 max-w-7xl">
+      <DataTable columns={columns} data={getData || []} />
+    </div>
+  );
+}
