e3c6e367cd7bfb5021537fa6b5a204ae8e4d0733
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-06-20 06:19:32 +0500

    added option to add color

---
+++ client/src/components/admin/Color.tsx
@@ -0,0 +1,81 @@
+import { ChangeEvent, useState } from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+interface Color {
+  id: number;
+  name: string;
+}
+
+const ProductColor = ({
+  color,
+  setcolor,
+}: {
+  color: Color[];
+  setcolor: React.Dispatch<React.SetStateAction<Color[]>>;
+}) => {
+  const [newColorName, setNewColorName] = useState<string>("");
+
+  const handleAddVariant = () => {
+    if (newColorName.trim() !== "") {
+      const newVariant: Color = {
+        id: color.length + 1, // Generate a unique ID for the variant
+        name: newColorName,
+      };
+      setcolor([...color, newVariant]);
+      setNewColorName("");
+    }
+  };
+
+  const handleRemoveVariant = (id: number) => {
+    const updatedcolor = color.filter((variant) => variant.id !== id);
+    setcolor(updatedcolor);
+  };
+
+  return (
+    <div className="space-y-4 flex flex-col mt-2 mb-3">
+      <div className="flex space-x-4">
+        <Input
+          type="text"
+          value={newColorName}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setNewColorName(e.target.value)
+          }
+          placeholder="Enter Color Code"
+        />
+      </div>
+      <Button type="button" onClick={handleAddVariant} variant={"default"}>
+        Add Color
+      </Button>
+      <div className="space-y-2">
+        {color.map((variant) => (
+          <div
+            key={variant.id}
+            className="flex justify-between items-center px-4 py-2 bg-gray-100 rounded-md"
+          >
+            <div>
+              <span className="font-medium flex gap-3 justify-center items-center">
+                {variant.name}
+                <span
+                  className="rounded-full w-3 h-3 block"
+                  style={{ background: variant.name }}
+                >
+                  &nbsp;
+                </span>
+              </span>
+            </div>
+            <Button
+              type="button"
+              onClick={() => handleRemoveVariant(variant.id)}
+              variant={"destructive"}
+            >
+              Remove
+            </Button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductColor;
