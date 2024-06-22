e6ba454ced59add31c0b9ae0ccdcaa73cb1d8f73
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-06-22 23:08:50 +0500

    get data using props and show it

--- client/src/components/product/Details.tsx
+++ client/src/components/product/Details.tsx
@@ -1,27 +1,47 @@
-function Details() {
+import { Product } from "@/lib/redux/types";
+
+function Details({ data }: { data: Product }) {
   return (
     <div className="max-w-6xl mx-auto px-8 py-4 max-sm:px-4  bg-background">
       <h1 className="text-2xl font-semibold mb-8">Details</h1>
       <p className="text-gray-400 text-sm tracking-wide mb-8">
-        Just as a book is judged by its cover, the first thing you notice when
-        you pick up a modern smartphone is the display. Nothing surprising,
-        because advanced technologies allow you to practically level the display
-        frames and cutouts for the front camera and speaker, leaving no room for
-        bold design solutions. And how good that in such realities Apple
-        everything is fine with displays. Both critics and mass consumers always
-        praise the quality of the picture provided by the products of the
-        Californian brand. And last year's 6.7-inch Retina panels, which had
-        ProMotion, caused real admiration for many.
+        {data.description}
       </p>
       <h1 className="text-2xl font-semibold mb-8">Screen</h1>
       <div className="w-full">
         <div className="flex w-full justify-between items-center text-sm border-b border-gray-300 mb-6 pb-2">
           <h2>Screen Diagonal</h2>
-          <p>6.7"</p>
+          <p>{data.screenSize}" inch</p>
         </div>
-        <div className="flex w-full justify-between items-center text-sm border-b border-gray-300 pb-2">
-          <h2>Screen Resolution</h2>
-          <p>2160x1620</p>
+      </div>
+      <h1 className="text-2xl font-semibold mb-8">Hardware</h1>
+      <div className="w-full">
+        <div className="flex w-full justify-between items-center text-sm border-b border-gray-300 mb-6 pb-2">
+          <h2>Chipset</h2>
+          <p>{data.cpu}</p>
+        </div>
+        <div className="flex w-full justify-between items-center text-sm border-b border-gray-300 mb-6 pb-2">
+          <h2>Cores</h2>
+          <p>{data.cores} cores</p>
+        </div>
+        <div className="flex w-full justify-between items-center text-sm border-b border-gray-300 mb-6 pb-2">
+          <h2>Ram</h2>
+          <p>{data.ram}GB</p>
+        </div>
+        <div className="flex w-full justify-between items-center text-sm border-b border-gray-300 mb-6 pb-2">
+          <h2>Battery</h2>
+          <p>{data.battery}mAh</p>
+        </div>
+      </div>
+      <h1 className="text-2xl font-semibold mb-8">Camera</h1>
+      <div className="w-full">
+        <div className="flex w-full justify-between items-center text-sm border-b border-gray-300 mb-6 pb-2">
+          <h2>Main Camera</h2>
+          <p>{data.mainCamera}</p>
+        </div>
+        <div className="flex w-full justify-between items-center text-sm border-b border-gray-300 mb-6 pb-2">
+          <h2>Front Camera</h2>
+          <p>{data.frontCamera}</p>
         </div>
       </div>
     </div>
