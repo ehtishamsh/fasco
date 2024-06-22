43bd997545e660fe8d044dc2a84baa113b2b084b
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-06-22 23:09:35 +0500

    get data from props and show it

--- client/src/components/product/TopDetailsGrid.tsx
+++ client/src/components/product/TopDetailsGrid.tsx
@@ -1,3 +1,4 @@
+import { Product } from "@/lib/redux/types";
 import { BsPhoneFill } from "react-icons/bs";
 import { BsCpu } from "react-icons/bs";
 import { GoCpu } from "react-icons/go";
@@ -5,73 +6,77 @@ import { IoMdCamera } from "react-icons/io";
 import { IoMdReverseCamera } from "react-icons/io";
 import { PiBatteryVerticalHighBold } from "react-icons/pi";
 
-const data = [
-  {
-    Icon: (
-      <BsPhoneFill size={24} className="text-gray-800/75 max-sm:text-base" />
-    ),
-    label: "Screen Size",
-    value: '6.5"',
-  },
-  {
-    Icon: <BsCpu size={24} className="text-gray-800/75 max-sm:text-base" />,
-    label: "CPU",
-    value: "A16 Bionic",
-  },
-  {
-    Icon: <GoCpu size={24} className="text-gray-800/75 max-sm:text-base" />,
-    label: "Cores",
-    value: "6",
-  },
-  {
-    Icon: (
-      <IoMdCamera size={24} className="text-gray-800/75 max-sm:text-base" />
-    ),
-    label: "Main Camera",
-    value: "48MP",
-  },
-  {
-    Icon: (
-      <IoMdReverseCamera
-        size={24}
-        className="text-gray-800/75 max-sm:text-base"
-      />
-    ),
-    label: "Front Camera",
-    value: "12MP",
-  },
-  {
-    Icon: (
-      <PiBatteryVerticalHighBold
-        size={24}
-        className="text-gray-800/75 max-sm:text-base"
-      />
-    ),
-    label: "Battery",
-    value: "5000mAh",
-  },
-];
-function TopDetailsGrid() {
-  // Data array containing icon components and corresponding data
-
+function TopDetailsGrid({ data }: { data: Product }) {
   return (
     <div className="grid grid-cols-3 max-sm:grid-cols-2 max-sm:gap-3 gap-4 mt-6">
-      {/* Mapping over the data array to render each item */}
-      {data.map((item, index) => (
-        <div
-          key={index}
-          className="flex justify-start items-center gap-2 p-3 bg-gray-200/50 rounded-md"
-        >
-          <span>
-            {/* Dynamically rendering the icon component */}
-            {item.Icon}
-          </span>
-          <div className="flex flex-col text-sm  max-md:text-xs">
-            <span className="text-gray-400">{item.label}</span>
-            <span>{item.value}</span>
-          </div>
+      <div className="flex justify-start items-center gap-2 p-3 bg-gray-200/50 rounded-md">
+        <span>
+          <BsPhoneFill
+            size={24}
+            className="text-gray-800/75 max-sm:text-base"
+          />
+        </span>
+        <div className="flex flex-col text-sm max-md:text-xs">
+          <span className="text-gray-400">Screen Size</span>
+          <span>{data.screenSize}"</span>
         </div>
-      ))}
+      </div>
+
+      <div className="flex justify-start items-center gap-2 p-3 bg-gray-200/50 rounded-md">
+        <span>
+          <BsCpu size={24} className="text-gray-800/75 max-sm:text-base" />
+        </span>
+        <div className="flex flex-col text-sm max-md:text-xs">
+          <span className="text-gray-400">CPU</span>
+          <span>{data.cpu}</span>
+        </div>
+      </div>
+
+      <div className="flex justify-start items-center gap-2 p-3 bg-gray-200/50 rounded-md">
+        <span>
+          <GoCpu size={24} className="text-gray-800/75 max-sm:text-base" />
+        </span>
+        <div className="flex flex-col text-sm max-md:text-xs">
+          <span className="text-gray-400">Cores</span>
+          <span>{data.cores}</span>
+        </div>
+      </div>
+
+      <div className="flex justify-start items-center gap-2 p-3 bg-gray-200/50 rounded-md">
+        <span>
+          <IoMdCamera size={24} className="text-gray-800/75 max-sm:text-base" />
+        </span>
+        <div className="flex flex-col text-sm max-md:text-xs">
+          <span className="text-gray-400">Main Camera</span>
+          <span>{data.mainCamera}</span>
+        </div>
+      </div>
+
+      <div className="flex justify-start items-center gap-2 p-3 bg-gray-200/50 rounded-md">
+        <span>
+          <IoMdReverseCamera
+            size={24}
+            className="text-gray-800/75 max-sm:text-base"
+          />
+        </span>
+        <div className="flex flex-col text-sm max-md:text-xs">
+          <span className="text-gray-400">Front Camera</span>
+          <span>{data.frontCamera}</span>
+        </div>
+      </div>
+
+      <div className="flex justify-start items-center gap-2 p-3 bg-gray-200/50 rounded-md">
+        <span>
+          <PiBatteryVerticalHighBold
+            size={24}
+            className="text-gray-800/75 max-sm:text-base"
+          />
+        </span>
+        <div className="flex flex-col text-sm max-md:text-xs">
+          <span className="text-gray-400">Battery</span>
+          <span>{data.battery}</span>
+        </div>
+      </div>
     </div>
   );
 }
