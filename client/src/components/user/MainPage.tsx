d778821926df9bff439cf07c300c2fb461265edd
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-06-28 23:56:32 +0500

    added table

--- client/src/components/user/MainPage.tsx
+++ client/src/components/user/MainPage.tsx
@@ -1,5 +1,6 @@
 import { User } from "@/lib/redux/types";
 import { Link } from "react-router-dom";
+import { RecentOrderTable } from "./RecentOrderTable";
 
 function MainPage() {
   const user: User = JSON.parse(localStorage.getItem("user") || "{}");
@@ -11,7 +12,7 @@ function MainPage() {
         </h1>
       </div>
       <div className="mt-4 grid grid-cols-3 gap-4 max-sm:gap-2 max-sm:grid-cols-1">
-        <div className="flex flex-col gap-3 p-4 max-sm:p-2 max-sm:justify-center border border-border rounded-lg">
+        <div className="flex flex-col gap-3 p-4 max-sm:p-2 max-sm:justify-center border border-gray-300/85 rounded-lg">
           <h1 className="text-sm">
             Personal Profile <span className="text-gray-400 text-sm"> | </span>
             <Link to={"/profile"} className="text-yellow-600">
@@ -25,8 +26,8 @@ function MainPage() {
             <h1 className="text-sm">{user.email}</h1>
           </div>
         </div>
-        <div className="col-span-2 grid grid-cols-2 border border-border rounded-lg ">
-          <div className="flex flex-col gap-3 p-4 max-sm:p-2 max-sm:justify-center border-r border-border">
+        <div className="col-span-2 max-sm:col-span-1 max-sm:grid-cols-1 grid grid-cols-2 border border-gray-300/85 rounded-lg ">
+          <div className="flex flex-col gap-3 p-4 max-sm:p-2 max-sm:justify-center border-r border-gray-300/85 max-sm:border-b">
             <h1 className="text-sm">
               Address Book<span className="text-gray-400 text-sm"> | </span>
               <Link to={"/profile"} className="text-yellow-600">
@@ -48,10 +49,10 @@ function MainPage() {
             </div>
           </div>
           <div className="flex flex-col gap-3 p-4 max-sm:p-2 max-sm:justify-center">
-            <span className="text-gray-400 text-xs mt-[30px]">
+            <span className="text-gray-400 text-xs mt-[30px] max-sm:mt-0">
               DEFAULT BILLING ADDRESS
             </span>
-            <div className="mt-2">
+            <div className="mt-2 max-sm:mt-0">
               <h1 className="text-sm mb-2 font-semibold">
                 {user.firstname} {user.lastname}
               </h1>
@@ -63,6 +64,13 @@ function MainPage() {
             </div>
           </div>
         </div>
+      </div>
+      <div className="mt-6 border border-gray-300/85 rounded-lg p-4">
+        <h1 className="border-b border-gray-300/85 pb-2">Recent Orders</h1>
+        {/* <div className="mb-6 my-8 flex justify-center items-center">
+          <h1 className="font-semibold">No Orders</h1>
+        </div> */}
+        <RecentOrderTable />
       </div>
     </div>
   );
