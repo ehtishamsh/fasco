d787d9d32ad0db25b56a3f31feb417dca15e2240
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-06-30 23:36:24 +0500

    changed links and icons

--- client/src/components/user/Sidebar.tsx
+++ client/src/components/user/Sidebar.tsx
@@ -1,10 +1,12 @@
 import { AvatarIcon, DashboardIcon } from "@radix-ui/react-icons";
-import { Box, Boxes, Notebook, User2Icon } from "lucide-react";
+import { Boxes, Notebook, User2Icon } from "lucide-react";
 import { GoChecklist } from "react-icons/go";
 import NavLinkItem from "../admin/NavlinkItem";
+import { FaAddressBook } from "react-icons/fa";
+import { User } from "@/lib/redux/types";
 
 function Sidebar() {
-  const userData = JSON.parse(localStorage.getItem("user") || "{}");
+  const userData: User = JSON.parse(localStorage.getItem("user") || "{}");
   return (
     <nav className="relative  border-r lg:block w-72  max-md:hidden ">
       <div className="space-y-4 py-2">
@@ -13,17 +15,17 @@ function Sidebar() {
             <div className="p-2 flex justify-center flex-col gap-2 items-center">
               <AvatarIcon className="w-20 h-20 text-muted-foreground" />
               <h1 className="text-lg max-sm:text-base">
-                Hello,{userData.name}
+                Hello,{userData.firstname + " " + userData.lastname}
               </h1>
             </div>
             <nav className="grid items-start">
-              <NavLinkItem to="/admin" Icon={DashboardIcon} label="Dashboard" />
               <NavLinkItem
-                to="/admin/orders"
-                Icon={GoChecklist}
-                label="Orders"
+                to="/dashboard"
+                Icon={DashboardIcon}
+                label="Dashboard"
               />
-              <NavLinkItem to="/admin/products" Icon={Box} label="Products" />
+              <NavLinkItem to="/orders" Icon={GoChecklist} label="Orders" />
+              <NavLinkItem to="/address" Icon={FaAddressBook} label="Address" />
               <NavLinkItem to="/admin/users" Icon={User2Icon} label="Users" />
               <NavLinkItem
                 to="/admin/categories"
