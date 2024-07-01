0b524c1effe86ef337279c21738bb8ff07f32d90
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-01 23:33:56 +0500

    update user

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import userService from "../services/userService";
+import { UpdateUser } from "../services/User";
+import { generateToken } from "../utils/auth";
 
 export const getUsers = async (req: Request, res: Response) => {
   try {
@@ -8,6 +10,35 @@ export const getUsers = async (req: Request, res: Response) => {
       res.status(404).send("No users found");
     }
     res.json(users);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { id, firstname, lastname, email, gender, dob } = req.body;
+    const user = await UpdateUser({
+      id,
+      firstname,
+      lastname,
+      email,
+      gender,
+      birthday: dob,
+      updatedAt: new Date(),
+    });
+
+    if (!user) {
+      res.status(404).send("User not found");
+    }
+    const token = await generateToken(user);
+    const removePassword = { ...user, password: undefined };
+    res.json({
+      status: 200,
+      message: "User updated successfully",
+      user: removePassword,
+      token,
+    });
   } catch (error) {
     console.log("error", error);
   }
