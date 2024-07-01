db6904e7a9a0a675ab5602c30cf4e80e1803f20e
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-01 23:34:09 +0500

    added edit route

--- server/src/routes/addressRoutes.ts
+++ server/src/routes/addressRoutes.ts
@@ -5,6 +5,7 @@ import {
   DELETEADDRESS,
   GETSINGLEADDRESS,
   GETUSERADDRESS,
+  UPDATEADDRESS,
 } from "../controllers/addressController";
 
 const router = Router();
@@ -13,6 +14,7 @@ router.get("/address", ALLADDRESS);
 router.get("/address/:id", GETSINGLEADDRESS);
 router.get("/address/user/:id", GETUSERADDRESS);
 router.post("/address/new", CREATEADDRESS);
+router.put("/address/edit", UPDATEADDRESS);
 router.delete("/address/delete", DELETEADDRESS);
 
 export default router;
