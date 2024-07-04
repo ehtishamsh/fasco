f0672df176242bf33557727b29dbff7c56d47a6a
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-04 23:39:34 +0500

    order detail page

---
+++ client/src/pages/user/OrderSinglePage.tsx
@@ -0,0 +1,11 @@
+import OrderDetail from "@/components/user/OrderDetail";
+
+function OrderSinglePage() {
+  return (
+    <>
+      <OrderDetail />
+    </>
+  );
+}
+
+export default OrderSinglePage;
