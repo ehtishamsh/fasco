49dbdf6cf60842c614b95a3b49141dfcf3c3526a
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-08 23:51:56 +0500

    added navbar and props to it

--- client/src/components/user/Dashboard.tsx
+++ client/src/components/user/Dashboard.tsx
@@ -1,9 +1,12 @@
 import Sidebar from "./Sidebar";
 import { Toaster } from "../ui/toaster";
 import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar";
+import { useState } from "react";
 
 function Dashboard() {
   const checkAdmin = JSON.parse(localStorage.getItem("user") || "{}");
+  const [hide, setHide] = useState(false);
   const token = localStorage.getItem("token");
   if (!token) {
     window.location.href = "/signin";
@@ -11,10 +14,12 @@ function Dashboard() {
   if (!checkAdmin) {
     window.location.href = "/";
   }
+
   return (
     <>
+      <Navbar hide={hide} sethide={setHide} />
       <main className="flex overflow-hidden">
-        <Sidebar />
+        <Sidebar hide={hide} />
         <div className="w-full  px-5  max-sm:px-2  mb-20 pt-10">
           <Outlet />
         </div>
