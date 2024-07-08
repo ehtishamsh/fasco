86cd1106e8c9a84efc98337058a1dc82719f4930
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-08 23:52:14 +0500

    animated the sidebar

--- client/src/components/user/Sidebar.tsx
+++ client/src/components/user/Sidebar.tsx
@@ -5,46 +5,65 @@ import NavLinkItem from "../admin/NavlinkItem";
 import { FaAddressBook } from "react-icons/fa";
 import { User } from "@/lib/redux/types";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
-function Sidebar() {
+function Sidebar({ hide }: { hide: boolean }) {
   const userData: User = JSON.parse(localStorage.getItem("user") || "{}");
   return (
-    <nav className="relative  border-r lg:block w-72  max-md:hidden ">
-      <div className="space-y-4 py-2">
-        <div className="px-3 py-2">
-          <div className="space-y-1">
-            <div className="p-2 flex justify-center flex-col gap-2 items-center">
-              <AvatarIcon className="w-20 h-20 text-muted-foreground" />
-              <h1 className="text-base max-sm:text-sm">
-                Hello, {userData.firstname + " " + userData.lastname}
-              </h1>
-              <Link
-                to="/profile"
-                className="text-sm max-sm:text-xs bg-yellow-200 rounded-lg py-1 px-2"
-              >
-                View Profile
-              </Link>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: hide ? 0 : 1,
+        x: hide ? -100 : 0,
+        width: hide ? 0 : 200,
+      }}
+      exit={{ width: 0 }}
+    >
+      <nav className="relative border-r lg:block max-md:hidden ">
+        <div className="space-y-4 py-2">
+          <div className="px-3 py-2">
+            <div className="space-y-1">
+              <div className="p-2 flex justify-center flex-col gap-2 items-center">
+                <AvatarIcon className="w-20 h-20 text-muted-foreground" />
+                <h1 className="text-base max-sm:text-sm">
+                  Hello, {userData.firstname + " " + userData.lastname}
+                </h1>
+                <Link
+                  to="/profile"
+                  className="text-sm max-sm:text-xs bg-yellow-200 rounded-lg py-1 px-2"
+                >
+                  View Profile
+                </Link>
+              </div>
+              <nav className="grid items-start">
+                <NavLinkItem
+                  to="/dashboard"
+                  Icon={DashboardIcon}
+                  label="Dashboard"
+                />
+                <NavLinkItem to="/orders" Icon={GoChecklist} label="Orders" />
+                <NavLinkItem
+                  to="/address"
+                  Icon={FaAddressBook}
+                  label="Address"
+                />
+                <NavLinkItem to="/admin/users" Icon={User2Icon} label="Users" />
+                <NavLinkItem
+                  to="/admin/categories"
+                  Icon={Boxes}
+                  label="Categories"
+                />
+                <NavLinkItem
+                  to="/admin/brands"
+                  Icon={Notebook}
+                  label="Brands"
+                />
+              </nav>
             </div>
-            <nav className="grid items-start">
-              <NavLinkItem
-                to="/dashboard"
-                Icon={DashboardIcon}
-                label="Dashboard"
-              />
-              <NavLinkItem to="/orders" Icon={GoChecklist} label="Orders" />
-              <NavLinkItem to="/address" Icon={FaAddressBook} label="Address" />
-              <NavLinkItem to="/admin/users" Icon={User2Icon} label="Users" />
-              <NavLinkItem
-                to="/admin/categories"
-                Icon={Boxes}
-                label="Categories"
-              />
-              <NavLinkItem to="/admin/brands" Icon={Notebook} label="Brands" />
-            </nav>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </motion.div>
   );
 }
 
