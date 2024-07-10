3e678f25f73bd67e9ff01514801dfd8a17fa425d
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-10 23:44:24 +0500

    added stripe

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,16 +7,21 @@ import uploadRoute from "./routes/uploadRoute";
 import addressRoute from "./routes/addressRoutes";
 import productRoutes from "./routes/productRoutes";
 import bodyParser from "body-parser";
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+///////////////////////////
 
 const app = express();
-
-// Cors
+app.use(express.static("public"));
 const cors = require("cors");
 app.use(bodyParser.json());
 app.use(cors());
 
+/////////////////////////
+
+// MIDDLEWARE
+
 app.use("/uploads", express.static("uploads"));
+
 // Routes
 app.use("/", userRoutes);
 app.use("/", uploadRoute);
@@ -24,23 +29,36 @@ app.use("/api", addressRoute);
 app.use("/api", productRoutes);
 app.use("/api", categoryRoute);
 app.use("/api", brandRoutes);
-// ERROR HANDLER
-app.post("/api/payment", async (req, res) => {
-  const { amount } = req.body;
 
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency: "usd",
-    });
+//Stripe
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+app.post("/api/create-checkout-session", async (req, res) => {
+  const products = req.body;
 
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    });
-  } catch (error) {
-    res.status(500).send(error);
-  }
+  const transformedProducts = products.products.map((product: any) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: product.title,
+        },
+        unit_amount: Number(product.price) * 100,
+      },
+      quantity: Number(product.quantity),
+    };
+  });
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: transformedProducts,
+    mode: "payment",
+    success_url: "http://localhost:5173/success",
+    cancel_url: "http://localhost:5173/cancel",
+  });
+
+  res.send({ id: session.id, url: session.url });
 });
+
+// ERROR HANDLER
 app.use(errorHandler);
 
 export default app;
