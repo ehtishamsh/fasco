aa00216751df47fc2876d1125d650a97418159d9
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-11 23:47:49 +0500

    check for session id if correct then show result

--- client/src/pages/Success.tsx
+++ client/src/pages/Success.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { CartState } from "@/lib/redux/types";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
 
 function Success() {
-  return <div>Success</div>;
+  const urlParams = new URLSearchParams(window.location.search);
+  const sessionId = urlParams.get("session_id");
+  const getProduct = useSelector((state: CartState) => state.cart.items);
+  const [checkPayment, setCheckPayment] = useState(false);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/api/check-session?session_id=${sessionId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.payment_status === "paid") {
+          setCheckPayment(true);
+        } else {
+          console.log("Payment not successful.");
+          setCheckPayment(false);
+        }
+      });
+  }, []);
+
+  return <div></div>;
 }
 
 export default Success;
