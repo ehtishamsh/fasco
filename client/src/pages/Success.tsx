a3991c92b83a2734b54a20197b13cb238c07bc5c
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-13 23:52:38 +0500

    check for errors

--- client/src/pages/Success.tsx
+++ client/src/pages/Success.tsx
@@ -1,13 +1,19 @@
 import { useToast } from "@/components/ui/use-toast";
 import { reset } from "@/lib/redux/cartSlice";
-import { CartState } from "@/lib/redux/types";
+import { CartState, User } from "@/lib/redux/types";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { motion } from "framer-motion";
 
 function Success() {
   const urlParams = new URLSearchParams(window.location.search);
   const sessionId = urlParams.get("session_id");
+  const user: User = JSON.parse(localStorage.getItem("user") || "{}");
   const [data, setData] = useState<any>({});
+  const [message, setMessage] = useState<any>({
+    status: 0,
+    message: "",
+  });
   const dispatch = useDispatch();
   const getProduct = useSelector((state: CartState) => state.cart.items);
   const [checkPayment, setCheckPayment] = useState(false);
@@ -25,9 +31,10 @@ function Success() {
           setCheckPayment(false);
         }
       });
-  }, []);
+  }, [sessionId]);
+
   useEffect(() => {
-    if (checkPayment === true) {
+    if (checkPayment) {
       const req = async () => {
         try {
           const body = {
@@ -49,19 +56,28 @@ function Success() {
           );
 
           const check = await response.json();
-          if (check.status === 200) {
-            toast({
-              title: "Success",
-              description: "Your order has been placed successfully",
-              variant: "success",
+          if (check.status) {
+            setMessage({
+              status: check.status,
+              message: check.message,
             });
+          }
+
+          if (check.status === 200) {
             dispatch(reset());
             setTimeout(() => {
               window.location.href = "/";
             }, 2000);
           }
+          if (check.status === 400) {
+            toast({
+              title: "Error",
+              description: check?.message,
+              variant: "destructive",
+            });
+            window.location.href = "/";
+          }
         } catch (error) {
-          console.log(error);
           toast({
             title: "Error",
             description: "Something went wrong",
@@ -72,19 +88,80 @@ function Success() {
       };
       req();
     }
-    return () => {};
   }, [checkPayment]);
 
   return (
-    <div>
-      {checkPayment && (
-        <>
-          <h1>Payment Successful</h1>
-          <h1>Your payment was successful</h1>
-          <h1>
-            Thank you for shopping with us {data.session.customer_details.name}
-          </h1>
-        </>
+    <div className="flex items-center justify-center h-[60svh]">
+      {checkPayment && message?.status === 200 ? (
+        <motion.div
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="p-10 bg-white rounded-lg shadow-lg text-center"
+        >
+          <motion.h1
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+            className="text-3xl font-bold text-green-500 mb-4"
+          >
+            Payment Successful
+          </motion.h1>
+          <motion.p
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+            className="text-lg mb-2"
+          >
+            Your payment was successful
+          </motion.p>
+          <motion.p
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.7, duration: 0.5 }}
+            className="text-lg"
+          >
+            Thank you for shopping with us, {user.firstname}
+          </motion.p>
+          <motion.span initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            redirecting to home page in 3 seconds...
+          </motion.span>
+        </motion.div>
+      ) : (
+        <motion.div
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="p-10 bg-white rounded-lg shadow-lg text-center"
+        >
+          <motion.h1
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+            className="text-3xl font-bold text-red-500 mb-4"
+          >
+            {message?.message || "Payment Failed"}
+          </motion.h1>
+          <motion.p
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+            className="text-lg mb-2"
+          >
+            Your payment was not successful
+          </motion.p>
+          <motion.p
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.7, duration: 0.5 }}
+            className="text-lg"
+          >
+            Please try again
+          </motion.p>
+          <motion.span initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            redirecting to home page in 3 seconds...
+          </motion.span>
+        </motion.div>
       )}
     </div>
   );
