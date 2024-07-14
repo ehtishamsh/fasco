9cf8ac1df1552fe7db07d64d2d28c8f618a67f73
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-14 23:22:47 +0500

    filter orders based on dates

--- client/src/components/user/SelectDate.tsx
+++ client/src/components/user/SelectDate.tsx
@@ -6,10 +6,46 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Order } from "@/lib/redux/types";
 
-export function SelectDate() {
+export function SelectDate({
+  setOrders,
+}: {
+  setOrders: React.Dispatch<React.SetStateAction<Order[] | undefined>>;
+}) {
+  const handleSelect = (value: string) => {
+    setOrders((prev) => {
+      const currentDate = new Date();
+      switch (value) {
+        case "5":
+          return prev?.slice(-5);
+        case "15d":
+        case "30d":
+        case "6m":
+          const daysMap: { [key: string]: number } = {
+            "15d": 15,
+            "30d": 30,
+            "6m": 180,
+          };
+          const daysLimit = daysMap[value];
+          return prev?.filter((order) => {
+            const orderDate = new Date(order.createdAt);
+            const dayDiff = Math.floor(
+              (currentDate.getTime() - orderDate.getTime()) /
+                (1000 * 60 * 60 * 24)
+            );
+            return dayDiff <= daysLimit;
+          });
+        case "all":
+          return prev;
+        default:
+          return prev;
+      }
+    });
+  };
+
   return (
-    <Select>
+    <Select onValueChange={handleSelect}>
       <SelectTrigger className="w-[180px]">
         <SelectValue placeholder="Last 5 order" />
       </SelectTrigger>
