4db7d4055887930fcd79913c734253d7cd5463ea
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-14 23:22:19 +0500

    loading

--- client/src/components/user/RecentOrderTable.tsx
+++ client/src/components/user/RecentOrderTable.tsx
@@ -10,12 +10,15 @@ import {
 import { Order, User } from "@/lib/redux/types";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Loading from "../ui/Loading";
 
 export function RecentOrderTable() {
   const [orders, setOrders] = useState<Order[]>();
+  const [loading, setLoading] = useState(false);
   const user: User = JSON.parse(localStorage.getItem("user") || "{}");
 
   useEffect(() => {
+    setLoading(true);
     const fetchData = async () => {
       try {
         const response = await fetch(
@@ -23,6 +26,7 @@ export function RecentOrderTable() {
         );
         const data = await response.json();
         setOrders(data.data);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -31,44 +35,62 @@ export function RecentOrderTable() {
     return () => {};
   }, []);
   return (
-    <Table>
-      <TableCaption>A list of your recent orders.</TableCaption>
-      <TableHeader>
-        <TableRow className="max-sm:text-xs">
-          <TableHead className="w-[100px]  max-sm:w-[70px]">Order #</TableHead>
-          <TableHead className="w-[100px] ">Placed On</TableHead>
-          <TableHead className="text-center">Images</TableHead>
-          <TableHead className="text-right">Total</TableHead>
-          <TableHead className="text-right">&nbsp;</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {orders &&
-          orders?.map((item, i) => (
-            <TableRow key={i} className="max-sm:text-xs">
-              <TableCell className="font-medium">{item?.orderNumber}</TableCell>
-              <TableCell>{new Date(item?.createdAt).toDateString()}</TableCell>
-              <TableCell className="flex gap-2 max-sm:flex-col text-center justify-center items-center ">
-                {item.items &&
-                  item.items.map((orderItems) => (
-                    <img
-                      src={`http://localhost:4000${orderItems.product.cover}`}
-                      className="h-12 w-12 max-sm:h-8 max-sm:w-8 "
-                    />
-                  ))}
-              </TableCell>
-              <TableCell className="text-right">USD ${item.amount}</TableCell>
-              <TableCell className="text-right">
-                <Link
-                  to={"/orders/view/" + item.orderNumber}
-                  className="bg-yellow-200 py-1 px-2 max-sm:px-1  max-sm:text-xs text-sm rounded-xl text-yellow-900"
-                >
-                  Manage
-                </Link>
-              </TableCell>
+    <>
+      {loading ? (
+        <Loading />
+      ) : orders && orders.length === 0 ? (
+        <div className="mb-6 my-8 flex justify-center items-center">
+          <h1 className="font-semibold">No Orders</h1>
+        </div>
+      ) : (
+        <Table>
+          <TableCaption>A list of your recent orders.</TableCaption>
+          <TableHeader>
+            <TableRow className="max-sm:text-xs">
+              <TableHead className="w-[100px]  max-sm:w-[70px]">
+                Order #
+              </TableHead>
+              <TableHead className="w-[100px] ">Placed On</TableHead>
+              <TableHead className="text-center">Images</TableHead>
+              <TableHead className="text-right">Total</TableHead>
+              <TableHead className="text-right">&nbsp;</TableHead>
             </TableRow>
-          ))}
-      </TableBody>
-    </Table>
+          </TableHeader>
+          <TableBody>
+            {orders &&
+              orders?.map((item, i) => (
+                <TableRow key={i} className="max-sm:text-xs">
+                  <TableCell className="font-medium">
+                    {item?.orderNumber}
+                  </TableCell>
+                  <TableCell>
+                    {new Date(item?.createdAt).toDateString()}
+                  </TableCell>
+                  <TableCell className="flex gap-2 max-sm:flex-col text-center justify-center items-center ">
+                    {item.items &&
+                      item.items.map((orderItems) => (
+                        <img
+                          src={`http://localhost:4000${orderItems.product.cover}`}
+                          className="h-12 w-12 max-sm:h-8 max-sm:w-8 "
+                        />
+                      ))}
+                  </TableCell>
+                  <TableCell className="text-right">
+                    USD ${item.amount}
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <Link
+                      to={"/orders/view/" + item.orderNumber}
+                      className="bg-yellow-200 py-1 px-2 max-sm:px-1  max-sm:text-xs text-sm rounded-xl text-yellow-900"
+                    >
+                      Manage
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      )}{" "}
+    </>
   );
 }
