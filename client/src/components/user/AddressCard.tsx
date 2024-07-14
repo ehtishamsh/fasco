2407be95fac442b7dadb5a3c34120bd1570a23e5
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-14 23:18:59 +0500

    removed map

--- client/src/components/user/AddressCard.tsx
+++ client/src/components/user/AddressCard.tsx
@@ -1,41 +1,41 @@
 import { Address } from "@/lib/redux/types";
 import { EditModal } from "./EditModal";
 
-function AddressCard({ address }: { address: Address[] }) {
-  return address.map((item) => (
+function AddressCard({ address }: { address: Address }) {
+  return (
     <div className="flex gap-2 text-sm items-center">
       <div className="flex flex-col gap-3 p-4 max-sm:p-2 max-sm:justify-center  w-full">
         <div className="flex justify-between items-center">
           <h1 className="text-sm">
-            {item.default ? "Main Address" : "Secondary Address"}
+            {address.default ? "Main Address" : "Secondary Address"}
             <span className="text-gray-400 text-sm"> | </span>
-            <EditModal address={item} />
+            <EditModal address={address} />
           </h1>
         </div>
         <span className="text-gray-400 text-xs">
-          {item.default
+          {address.default
             ? "Default Address"
-            : item.shipping
+            : address.shipping
             ? "Shipping Address"
-            : item.billing
+            : address.billing
             ? "Billing Address"
             : ""}
         </span>
         <div className="mt-2">
           <h1 className="text-sm mb-2 font-semibold">
-            {item.firstname} {item.lastname}
+            {address.firstname} {address.lastname}
           </h1>
           <div className="text-xs flex flex-col gap-2">
-            <span>{item.addressLine1.replace(/\s+/g, " ")}</span>
+            <span>{address.addressLine1.replace(/\s+/g, " ")}</span>
             <span>
-              {item.city} - {item.state} - {item.country}
+              {address.city} - {address.state} - {address.country}
             </span>
-            <span>{item.postalCode}</span>
+            <span>{address.postalCode}</span>
           </div>
         </div>
       </div>
     </div>
-  ));
+  );
 }
 
 export default AddressCard;
