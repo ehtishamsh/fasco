57e2504b3bc176070dd9399f52007090cd85bb74
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-16 23:41:00 +0500

    added more props

--- client/src/components/user/ItemCard.tsx
+++ client/src/components/user/ItemCard.tsx
@@ -1,58 +1,90 @@
+import { Color, Variant } from "@/lib/redux/types";
 import { Link } from "react-router-dom";
+import OrderCancel from "./OrderCancel";
 
-interface Props {
-  orderNumber: number;
-  images: string[];
+const ItemCard = ({
+  cover,
+  title,
+  quantity,
+  price,
+  orderNumber,
+  selectedColor,
+  selectedVariant,
+  category,
+  brand,
+  deliverd,
+  confirmed,
+}: {
+  cover: string;
   title: string;
   quantity: number;
-  price: number;
-}
-const ItemCard = ({ images, title, quantity, price, orderNumber }: Props) => {
+  price: string;
+  orderNumber: number;
+  selectedColor: Color;
+  selectedVariant: Variant;
+  category: string;
+  brand: string;
+  deliverd: boolean;
+  confirmed: boolean;
+}) => {
   return (
     <div className="">
       <div className="mt-3 grid grid-cols-4 max-sm:grid-cols-5 max-sm:gap-5 gap-2 px-6 py-2 text-sm max-sm:px-2">
         <div className="text-left w-full flex gap-1 col-span-1 max-sm:col-span-2">
-          {images.map((src, index) => (
-            <img
-              key={index}
-              src={src}
-              alt=""
-              className="w-20 h-20 max-sm:w-full max-sm:h-full max-sm:object-contain"
-            />
-          ))}
+          <img
+            src={`http://localhost:4000${cover}`}
+            alt=""
+            className="w-20 h-20 max-sm:w-full max-sm:h-full max-sm:object-contain"
+          />
         </div>
         <div className="col-span-3 max-sm:flex max-sm:flex-col grid grid-cols-4">
           <Link
-            to={`/smartphones/oneplus/${title.replace(" ", "-").toLowerCase()}`}
-            className="text-gray-400"
+            to={`/${category?.toLowerCase() || ""}/${
+              brand?.toLowerCase() || ""
+            }/${title.replace(/\s+/g, "-").toLowerCase()}`}
+            className="text-gray-700 flex flex-col"
           >
+            <span className="text-gray-400 text-xs">{brand}</span>
             {title}
+            <span
+              style={{ backgroundColor: selectedColor.color }}
+              className="w-3 h-3 rounded-full"
+            >
+              &nbsp;
+            </span>
+            <span className="text-gray-400 text-xs">
+              {selectedVariant.variant} | ${selectedVariant.price}
+            </span>
           </Link>
-          <div className=" max-sm:mt-4 font-semibold">
+          <div className=" max-sm:mt-4 font-semibold h-full flex  items-center">
             <span>${price}</span>
           </div>
-          <div className=" max-sm:mt-4">
+          <div className=" max-sm:mt-4 h-full flex  items-center">
             <span>
               <span className="text-gray-400">Qty:</span> {quantity}
             </span>
           </div>
           <div className="flex flex-col max-sm:mt-4 ">
-            <Link
-              to={`/order/review/${orderNumber}`}
-              className="text-yellow-600"
-            >
-              Review
-            </Link>
-            <span>-</span>
-            <Link
-              to={`/order/cancel/${orderNumber}`}
-              className="text-yellow-600 flex flex-col  "
-            >
-              Cancel
+            {deliverd && (
+              <>
+                <Link
+                  to={`/order/review/${orderNumber}`}
+                  className="text-yellow-600"
+                >
+                  Review
+                </Link>
+                <span>-</span>
+              </>
+            )}
+            <div className="text-yellow-600 flex flex-col  ">
+              <OrderCancel
+                orderConfirmed={confirmed}
+                orderNumber={orderNumber}
+              />
               <span className="text-gray-400 text-xs">
                 Should be under 2 days
               </span>
-            </Link>
+            </div>
           </div>
         </div>
       </div>
