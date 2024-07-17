0e56f2aad6b6da1cfabd430bc5911777a79adcc0
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-17 23:09:27 +0500

    update routes

--- server/src/routes/orderRouter.ts
+++ server/src/routes/orderRouter.ts
@@ -10,4 +10,5 @@ const router = Router();
 router.post("/order/create", createOrderController);
 router.get("/order/user/:id", getUserOrdersController);
 router.get("/order/:id", getOrderDetail);
+router.put("/order/update");
 export default router;
