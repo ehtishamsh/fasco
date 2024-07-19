461bc29ca49dcfb733bd65055387dc169b31c12d
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-19 06:36:27 +0500

    added open and close functionality

--- client/src/components/user/Sidebar.tsx
+++ client/src/components/user/Sidebar.tsx
@@ -6,20 +6,37 @@ import { FaAddressBook } from "react-icons/fa";
 import { User } from "@/lib/redux/types";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 function Sidebar({ hide }: { hide: boolean }) {
   const userData: User = JSON.parse(localStorage.getItem("user") || "{}");
+  const [checkWidth, setCheckWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      setCheckWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [checkWidth]);
+
   return (
     <motion.div
+      className={` ${
+        checkWidth < 700
+          ? "fixed z-50 right-0 top-0 bg-white h-screen pt-20"
+          : "h-full"
+      }`}
       initial={{ opacity: 0 }}
       animate={{
         opacity: hide ? 0 : 1,
         x: hide ? -100 : 0,
-        width: hide ? 0 : 200,
+        width: hide ? 0 : checkWidth < 700 ? "100%" : 300,
       }}
-      exit={{ width: 0 }}
+      exit={{ width: 0, display: "none" }}
     >
-      <nav className="relative border-r lg:block max-md:hidden ">
+      <nav className="relative h-full border-r lg:block w-full">
         <div className="space-y-4 py-2">
           <div className="px-3 py-2">
             <div className="space-y-1">
