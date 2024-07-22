d313c7f806ea2547b698fecf22a0147de78fb95a
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-07-22 23:18:21 +0500

    orders table

---
+++ client/src/components/admin/orders/OrdersTable.tsx
@@ -0,0 +1,25 @@
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
+import { useEffect, useState } from "react";
+
+export default function ProductsTable() {
+  const [getData, setData] = useState<any>({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const req = await fetch("http://localhost:4000/api/products");
+        const res = await req.json();
+        setData(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className=" mx-auto pb-8 max-w-7xl">
+      <DataTable columns={columns} data={getData?.products || []} />
+    </div>
+  );
+}
