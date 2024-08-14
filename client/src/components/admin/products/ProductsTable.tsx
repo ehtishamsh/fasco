9ca2af5f969f7b7bd7d6d6262ac0ca6f521fc145
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-08-14 23:39:30 +0500

    add option to filter data based on cate

--- client/src/components/admin/products/ProductsTable.tsx
+++ client/src/components/admin/products/ProductsTable.tsx
@@ -1,10 +1,30 @@
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
 import { useEffect, useState } from "react";
+import Select from "../Select";
 
+interface Option {
+  id: string;
+  name: string;
+}
 export default function ProductsTable() {
   const [getData, setData] = useState<any>({});
+  const [categories, setCategories] = useState<Option[]>([]);
+  const [selectCategory, setSelectCategory] = useState<any>([]);
 
+  useEffect(() => {
+    setData((prev: any) => {
+      return {
+        ...prev,
+        products:
+          selectCategory?.id === undefined
+            ? getData?.products
+            : getData?.products?.filter(
+                (item: any) => selectCategory.id === item.categoryId
+              ),
+      };
+    });
+  }, [selectCategory]);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -15,10 +35,30 @@ export default function ProductsTable() {
         console.log(error);
       }
     };
+
     fetchData();
   }, []);
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const req = await fetch("http://localhost:4000/api/categories");
+        const res = await req.json();
+        setCategories(res.categories);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCategories();
+  }, []);
+
   return (
-    <div className=" pb-8">
+    <div className="pb-8">
+      <Select
+        name="Category"
+        options={categories}
+        selectedOptions={selectCategory}
+        setSelectedOptions={setSelectCategory}
+      />
       <DataTable columns={columns} data={getData?.products || []} />
     </div>
   );
