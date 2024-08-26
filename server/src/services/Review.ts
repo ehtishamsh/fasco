0b69c2584d028dca13d9d9276ec73a10f41fb247
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-08-26 23:25:08 +0500

    review

---
+++ server/src/services/Review.ts
@@ -0,0 +1,17 @@
+import prisma from "../utils/db";
+
+export async function getAll() {
+  return await prisma.review.findMany();
+}
+export async function getOne(userid: string, productid: string) {
+  return await prisma.review.findMany({
+    where: {
+      userId: userid,
+      productId: productid,
+    },
+  });
+}
+
+export async function create(data: any) {
+  return await prisma.review.create({ data });
+}
