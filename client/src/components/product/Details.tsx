3a95a729ff84344ec7e69b2a201f4454362a64ff
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-08-26 23:23:57 +0500

    added headings

--- client/src/components/product/Details.tsx
+++ client/src/components/product/Details.tsx
@@ -5,63 +5,92 @@ interface FieldMapping {
   unit?: string;
 }
 
-const DetailsFieldMappings: { [key: string]: FieldMapping } = {
-  screenSize: { label: "Screen Diagonal", unit: "inch" },
-  cpu: { label: "Chipset" },
-  ram: { label: "Ram", unit: "GB" },
-  cores: { label: "Cores", unit: "cores" },
-  mainCamera: { label: "Main Camera" },
-  frontCamera: { label: "Front Camera" },
-  battery: { label: "Battery", unit: "mAh" },
-  features: { label: "Features" },
-  connectivity: { label: "Connectivity" },
-  sensor: { label: "Sensor" },
-  screenType: { label: "Screen Type" },
-  lens: { label: "Lens" },
-  zoom: { label: "Zoom" },
-  megapixels: { label: "Megapixels" },
-  aperture: { label: "Aperture" },
-  videoResolution: { label: "Video Resolution" },
-  type: { label: "Type" },
-  noiseCancellation: { label: "Noise Cancellation", unit: "Yes" },
-  batteryLife: { label: "Battery Life" },
-  wireless: { label: "Wireless", unit: "Yes" },
-  microphone: { label: "Microphone", unit: "Yes" },
-  storage: { label: "Storage" },
-  gpu: { label: "GPU" },
-  maxResolution: { label: "Max Resolution" },
-  numberOfControllers: { label: "Number of Controllers" },
-  compatibleGames: { label: "Compatible Games" },
-};
+const DetailsFieldMappings: { [key: string]: { [key: string]: FieldMapping } } =
+  {
+    hardware: {
+      cpu: { label: "Chipset" },
+      ram: { label: "Ram", unit: "GB" },
+      gpu: { label: "GPU" },
+      cores: { label: "Cores", unit: "cores" },
+      storage: { label: "Storage" },
+    },
+    camera: {
+      mainCamera: { label: "Main Camera" },
+      frontCamera: { label: "Front Camera" },
+      megapixels: { label: "Megapixels" },
+      aperture: { label: "Aperture" },
+      videoResolution: { label: "Video Resolution" },
+      lens: { label: "Lens" },
+      zoom: { label: "Zoom" },
+    },
+    battery: {
+      battery: { label: "Battery", unit: "mAh" },
+      batteryLife: { label: "Battery Life" },
+    },
+    display: {
+      screenSize: { label: "Screen Diagonal", unit: "inch" },
+      screenType: { label: "Screen Type" },
+      maxResolution: { label: "Max Resolution" },
+    },
+    audio: {
+      noiseCancellation: { label: "Noise Cancellation", unit: "Yes" },
+      microphone: { label: "Microphone", unit: "Yes" },
+      wireless: { label: "Wireless", unit: "Yes" },
+    },
+    connectivityAndSensors: {
+      connectivity: { label: "Connectivity" },
+      sensor: { label: "Sensor" },
+    },
+    gaming: {
+      type: { label: "Type" },
+      numberOfControllers: { label: "Number of Controllers" },
+      compatibleGames: { label: "Compatible Games" },
+    },
+    features: {
+      features: { label: "Features" },
+    },
+  };
 
 function Details({ data }: { data: Product | any }) {
-  const filterKeys = Object.keys(DetailsFieldMappings).filter(
-    (field: string) =>
-      data[field] !== null &&
-      data[field] !== undefined &&
-      data[field] !== "" &&
-      data[field] !== 0 &&
-      data[field] !== false
-  );
-
   return (
     <div className="max-w-6xl mx-auto px-8 py-4 max-sm:px-4  bg-background">
       <span className="text-gray-400 inline-block my-10 text-sm ">
         {data?.description}
       </span>
       <h1 className="text-2xl font-semibold mb-8">Details</h1>
-      {filterKeys.map((field: string) => {
-        const { label, unit } =
-          DetailsFieldMappings[field as keyof typeof DetailsFieldMappings];
+
+      {Object.keys(DetailsFieldMappings).map((category) => {
+        const fields = DetailsFieldMappings[category];
+        const filterKeys = Object.keys(fields).filter(
+          (field: string) =>
+            data[field] !== null &&
+            data[field] !== undefined &&
+            data[field] !== "" &&
+            data[field] !== 0 &&
+            data[field] !== false
+        );
+
+        if (filterKeys.length === 0) return null;
+
         return (
-          <div key={field} className="w-full">
-            <div className="flex w-full justify-between items-center text-sm border-b border-gray-300 mb-6 pb-2">
-              <h2>{label}</h2>
-              <p className="max-w-96">
-                {data[field]}
-                {unit ? ` ${unit}` : ""}
-              </p>
-            </div>
+          <div key={category} className="mb-6">
+            <h2 className="text-xl font-semibold mb-4 capitalize">
+              {category}
+            </h2>
+            {filterKeys.map((field: string) => {
+              const { label, unit } = fields[field];
+              return (
+                <div key={field} className="w-full">
+                  <div className="flex w-full justify-between items-center text-sm border-b border-gray-300 mb-6 pb-2">
+                    <h3>{label}</h3>
+                    <p className="max-w-96">
+                      {data[field]}
+                      {unit ? ` ${unit}` : ""}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         );
       })}
