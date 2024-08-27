4a7558c5de862ef35ba82f9fb435fe740145c292
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-08-27 23:56:19 +0500

    review routes

--- server/src/routes/reviewRoutes.ts
+++ server/src/routes/reviewRoutes.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
-import { checkProductReview } from "../controllers/reviewController";
 
 const router = Router();
 
-router.get("/reviews/user/:id", checkProductReview);
 export default router;
