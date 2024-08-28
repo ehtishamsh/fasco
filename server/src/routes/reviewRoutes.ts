dc093a334d0c63e0aa4879046f3f55cd89e53451
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-08-28 23:56:49 +0500

    new routes

--- server/src/routes/reviewRoutes.ts
+++ server/src/routes/reviewRoutes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import { orderReview } from "../controllers/reviewController";
 
 const router = Router();
 
+router.get("/reviews/user/:uid", orderReview);
 export default router;
