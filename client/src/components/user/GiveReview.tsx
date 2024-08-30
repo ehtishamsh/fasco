0c98787b8f1cf2d238a418feb2ba86ee422f6d39
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-08-30 23:32:16 +0500

    post review

--- client/src/components/user/GiveReview.tsx
+++ client/src/components/user/GiveReview.tsx
@@ -3,11 +3,14 @@ import { BreadCrumbAdmin } from "../admin/BreadCrumAdmin";
 import { useEffect, useState } from "react";
 import { Order } from "@/lib/redux/types";
 import Loading from "../ui/Loading";
-import { StarIcon, Stars } from "lucide-react";
+
+import StarRating from "./StarRating";
 import { Button } from "../ui/button";
 
 function GiveReview() {
   const [orders, setOrders] = useState<Order>();
+  const [ratings, setRatings] = useState<{ [key: string]: number }>({});
+  const [comment, setComment] = useState<{ [key: string]: string }>({});
   const { id } = useParams();
   const [loading, setLoading] = useState(false);
   useEffect(() => {
@@ -50,56 +53,66 @@ function GiveReview() {
                   </span>
                 </div>
               </div>
-              <div className="mt-3 grid grid-cols-1 gap-4 px-6 py-2 ">
-                {orders?.items &&
-                  orders?.items.map((item) => (
-                    <>
-                      <div className="flex flex-col gap-2">
-                        <div className="flex justify-between gap-4 items-start">
-                          <span>
-                            <img
-                              src={"http://localhost:4000" + item.product.cover}
-                              alt=""
-                              className="w-16 h-16 rounded-lg"
-                            />
-                          </span>
-                          <span className="w-full   grid grid-cols-1  gap-2">
-                            <Link
-                              to={`/${
-                                typeof item.product.category === "object" &&
-                                item.product.category.name.toLowerCase()
-                              }/${
-                                typeof item.product.brand === "object" &&
-                                item.product.brand.name.toLowerCase()
-                              }/${item.product.slug}`}
-                              className="text-primary underline"
-                            >
-                              {item.product.title}
-                            </Link>
-                            <span className="text-xs text-gray-400 flex items-center gap-1">
-                              Color :{" "}
-                              <span
-                                className="w-[10px] h-[10px] inline-block rounded-full border border-muted-foreground"
-                                style={{ backgroundColor: item.color.color }}
-                              >
-                                &nbsp;
-                              </span>
-                              , Variant : {item.variant.variant}
+              <div>
+                <div className="mt-3 grid grid-cols-1 gap-4 px-6 py-2 ">
+                  {orders?.items &&
+                    orders?.items.map((item) => (
+                      <>
+                        <div className="flex flex-col gap-2">
+                          <div className="flex justify-between gap-4 items-start">
+                            <span>
+                              <img
+                                src={
+                                  "http://localhost:4000" + item.product.cover
+                                }
+                                alt=""
+                                className="w-16 h-16 rounded-lg"
+                              />
                             </span>
-                            <div className="flex items-center gap-1 mt-10">
-                              {[...Array(5)].map((_, index) => (
-                                <>
-                                  <Button variant="ghost" size="icon">
-                                    <StarIcon className="text-accent" />
-                                  </Button>
-                                </>
-                              ))}
-                            </div>
-                          </span>
+                            <span className="w-full   grid grid-cols-1  gap-2">
+                              <Link
+                                to={`/${
+                                  typeof item.product.category === "object" &&
+                                  item.product.category.name.toLowerCase()
+                                }/${
+                                  typeof item.product.brand === "object" &&
+                                  item.product.brand.name.toLowerCase()
+                                }/${item.product.slug}`}
+                                className="text-primary underline"
+                              >
+                                {item.product.title}
+                              </Link>
+                              <span className="text-xs text-gray-400 flex items-center gap-1">
+                                Color :{" "}
+                                <span
+                                  className="w-[10px] h-[10px] inline-block rounded-full border border-muted-foreground"
+                                  style={{ backgroundColor: item.color.color }}
+                                >
+                                  &nbsp;
+                                </span>
+                                , Variant : {item.variant.variant}
+                              </span>
+                              <div className="flex flex-col justify-center gap-1 mt-3">
+                                <span className="text-gray-500 text-xs mb-2">
+                                  Rating :
+                                </span>
+                                <StarRating
+                                  productId={item.product.id}
+                                  ratings={ratings}
+                                  setRatings={setRatings}
+                                  comment={comment}
+                                  setComment={setComment}
+                                />
+                              </div>
+                            </span>
+                          </div>
                         </div>
-                      </div>
-                    </>
-                  ))}
+                      </>
+                    ))}
+                </div>
+                <Button className="w-full" variant={"default"}>
+                  Give Review
+                </Button>
               </div>
             </div>
           </>
