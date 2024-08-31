c65c01dbf675c616b260cf5c26597d954e82ceb5
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-08-31 23:44:31 +0500

    create and get reviews routes

--- server/src/routes/reviewRoutes.ts
+++ server/src/routes/reviewRoutes.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
-import { orderReview } from "../controllers/reviewController";
+import {
+  addReview,
+  getReviewsByOrderID,
+  orderReview,
+} from "../controllers/reviewController";
 
 const router = Router();
 
 router.get("/reviews/user/:uid", orderReview);
+router.post("/reviews", addReview);
+router.get("/reviews/:id", getReviewsByOrderID);
 export default router;
