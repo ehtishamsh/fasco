32484d591baca4698d959285c1c6821f963ee0ad
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-08-31 23:45:04 +0500

    create and get reviews

--- server/src/controllers/reviewController.ts
+++ server/src/controllers/reviewController.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
-import { findUserByID } from "../services/User";
-import { getOrderByUserID } from "../services/Order";
-import { findProductBySlug } from "../services/Product";
-import { getReviewsByUserID } from "../services/Review";
+import { findUserByEmail, findUserByID } from "../services/User";
+import { getOrderByOrderNumber, getOrderByUserID } from "../services/Order";
+import { findProductById, findProductBySlug } from "../services/Product";
+import {
+  create,
+  getReviewsByUserID,
+  getReviewsByUserIDandProductID,
+} from "../services/Review";
+import { object } from "zod";
 
 export async function orderReview(req: Request, res: Response) {
   const { uid } = req.params;
@@ -79,6 +84,137 @@ export async function orderReview(req: Request, res: Response) {
   }
 }
 
-// export async function (req: Request, res: Response) {
-//   const { id } = req.params;
-// }
+export async function addReview(req: Request, res: Response) {
+  try {
+    const {
+      ratings,
+      comment,
+      userid,
+    }: {
+      ratings: { [key: string]: number };
+      comment: { [key: string]: string };
+      userid: string;
+    } = req.body;
+    const checkuser = await findUserByID(userid);
+    if (!checkuser) {
+      return res.status(404).json({
+        status: 404,
+        message: "User not found",
+      });
+    }
+    const getProductID = Object.keys(ratings);
+    const checkproducts = await Promise.all(
+      getProductID.map(async (id) => {
+        const product = await findProductById(id);
+        if (!product) {
+          return res.status(404).json({
+            status: 404,
+            message: "Product not found",
+          });
+        }
+        return product;
+      })
+    );
+    if (checkproducts.length === 0) {
+      return res.status(404).json({
+        status: 404,
+        message: "Product not found",
+      });
+    }
+    const createReviews = await Promise.all(
+      getProductID.map(async (id, index) => {
+        if (
+          id === Object.keys(ratings)[index] &&
+          id === Object.keys(comment)[index]
+        ) {
+          const review = ratings[id];
+          const comments = comment[id];
+          const createReview = await create({
+            userid,
+            productId: id,
+            comment: comments,
+            rating: review,
+          });
+          if (!createReview) {
+            return res.status(400).json({
+              status: 400,
+              message: "Bad request",
+            });
+          }
+          return createReview;
+        }
+      })
+    );
+    if (createReviews.length === 0) {
+      return res.status(400).json({
+        status: 400,
+        message: "Bad request",
+      });
+    }
+    return res.status(200).json({
+      status: 200,
+      message: "Review created successfully",
+      reviews: createReviews,
+    });
+  } catch (error) {
+    res.status(400).send("Bad request");
+  }
+}
+
+export async function getReviewsByOrderID(req: Request, res: Response) {
+  const { id } = req.params;
+
+  try {
+    const order = await getOrderByOrderNumber(Number(id));
+    if (!order) {
+      return res.status(404).json({ message: "Order not found", status: 404 });
+    }
+
+    const productID = order.items?.map((item) => item.product.id);
+    const getReviews = await Promise.all(
+      productID.map(async (id) => {
+        const userEmail = order.user.email;
+        const getUser = await findUserByEmail(userEmail);
+        if (!getUser) {
+          return res
+            .status(404)
+            .json({ message: "User not found", status: 404 });
+        }
+
+        const product = await findProductById(id);
+        if (!product) {
+          return res
+            .status(404)
+            .json({ message: "Product not found", status: 404 });
+        }
+
+        const review = await getReviewsByUserIDandProductID({
+          productid: id,
+          userid: getUser.id,
+        });
+        if (!review) {
+          return res
+            .status(404)
+            .json({ message: "Reviews not found", status: 404 });
+        }
+        return {
+          ...review,
+          color: order.items?.find((item) => item.product.id === id)?.color,
+          variant: order.items?.find((item) => item.product.id === id)?.variant,
+        };
+      })
+    );
+    if (getReviews.length > 0) {
+      return res.status(200).json({
+        message: "Reviews fetched successfully",
+        status: 200,
+        reviews: getReviews,
+      });
+    }
+    return res.status(200).json({ data: order, status: 200 });
+  } catch {
+    res
+      .status(400)
+      .json({ message: "Error fetching order items", status: 400 });
+  }
+}
