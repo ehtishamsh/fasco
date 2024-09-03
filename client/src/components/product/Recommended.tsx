0f1bedfa2e51822e0f962c9d4b3a4010b0a00768
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-09-03 23:54:51 +0500

    get products by category

--- client/src/components/product/Recommended.tsx
+++ client/src/components/product/Recommended.tsx
@@ -1,19 +1,23 @@
 import { useState, useEffect } from "react";
 import ShowcaseProducts from "../home/ShowcaseProducts";
-import { Product as Data } from "@/lib/redux/types";
+import { Product } from "@/lib/redux/types";
+import { useParams } from "react-router-dom";
 function Recommended() {
-  const [data, setData] = useState<Data[]>([]);
+  const [data, setData] = useState<Product[]>([]);
+  const { category } = useParams();
+  const firstCharacter = category?.split("").splice(0, 1);
+  const remainingCharacter = category?.split("").splice(1).join("");
+  const finalCategory =
+    firstCharacter && firstCharacter[0].toUpperCase() + remainingCharacter;
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const req = await fetch("https://dummyjson.com/products");
-        const res = await req.json();
-        const removetenProducts = res?.products.filter(
-          //@ts-ignore
-          (product: Data, i: number) => i < 4
+        const req = await fetch(
+          "http://localhost:4000/api/products/category/" + finalCategory
         );
-
-        setData(removetenProducts);
+        const res = await req.json();
+        const cut = res.data.slice(0, 4);
+        setData(cut);
       } catch (error) {
         console.log(error);
       }
