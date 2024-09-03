3614a6a6a3271a98db6f15e848488eaf18a62ea2
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-09-03 23:55:14 +0500

    user reviews

--- client/src/components/product/Reviews.tsx
+++ client/src/components/product/Reviews.tsx
@@ -1,135 +1,43 @@
+import { Reviews as ReviewType } from "@/lib/redux/types";
+import { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa6";
-function Reviews() {
+function Reviews({ reviewsData }: { reviewsData: ReviewType[] }) {
+  const [reviews, setReviews] = useState<ReviewType[]>(reviewsData);
+  useEffect(() => {
+    setReviews(reviewsData);
+  }, [reviewsData]);
+
   return (
     <div className="mt-8 grid grid-cols-1 gap-5">
-      <div className="flex p-4 gap-6 max-sm:gap-3 bg-gray-100 rounded-xl">
-        <div className="flex justify-center items-start max-w-[70px] max-sm:max-w-[50px]">
-          <img src="/review.jpg" alt="" className="rounded-full" />
-        </div>
-        <div className="w-full flex flex-col gap-3 max-sm:gap-2">
-          <div>
-            <div className="flex justify-between items-center">
-              <h1 className="font-semibold font-sm">James Harris</h1>
-              <span className="text-gray-500/45 font-semibold text-xs">
-                24 January,2023
-              </span>
+      {reviews.map((review) => (
+        <div className="flex p-4 gap-6 max-sm:gap-3 bg-gray-100 rounded-xl">
+          <div className="w-full flex flex-col gap-3 max-sm:gap-2">
+            <div>
+              <div className="flex justify-between items-center">
+                <h1 className="font-semibold font-sm">
+                  {review.user.firstname} {review.user.lastname}
+                </h1>
+                <span className="text-gray-500/45 font-semibold text-xs">
+                  {new Date(review.createdAt).toDateString()}
+                </span>
+              </div>
             </div>
-          </div>
-          <div className="flex gap-1">
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-          </div>
-          <p className="text-sm text-gray-500  max-sm:line-clamp-4">
-            I was a bit nervous to be buying a secondhand phone from Amazon, but
-            I couldn’t be happier with my purchase!! I have a pre-paid data plan
-            so I was worried that this phone wouldn’t connect with my data plan,
-            since the new phones don’t have the physical Sim tray anymore, but
-            couldn’t have been easier! I bought an Unlocked black iPhone 14 Pro
-            Max in excellent condition and everything is PERFECT. It was super
-            easy to set up and the phone works and looks great. It truly was in
-            excellent condition. Highly recommend!!!🖤
-          </p>
-        </div>
-      </div>
-      <div className="flex p-4 gap-6 max-sm:gap-3 bg-gray-100 rounded-xl">
-        <div className="flex justify-center items-start max-w-[70px] max-sm:max-w-[50px]">
-          <img src="/review.jpg" alt="" className="rounded-full" />
-        </div>
-        <div className="w-full flex flex-col gap-3 max-sm:gap-2">
-          <div>
-            <div className="flex justify-between items-center">
-              <h1 className="font-semibold font-sm">James Harris</h1>
-              <span className="text-gray-500/45 font-semibold text-xs">
-                24 January,2023
-              </span>
+            <div className="flex gap-1">
+              {[...Array(5)].map((_, i) => (
+                <FaStar
+                  key={i}
+                  className={`w-4 h-4 max-sm:w-3 max-sm:h-3 ${
+                    i < review.rating ? "text-yellow-500" : "text-gray-400"
+                  }`}
+                />
+              ))}
             </div>
+            <p className="text-sm text-gray-500  max-sm:line-clamp-4">
+              {review.comment}
+            </p>
           </div>
-          <div className="flex gap-1">
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-          </div>
-          <p className="text-sm text-gray-500  max-sm:line-clamp-4">
-            I was a bit nervous to be buying a secondhand phone from Amazon, but
-            I couldn’t be happier with my purchase!! I have a pre-paid data plan
-            so I was worried that this phone wouldn’t connect with my data plan,
-            since the new phones don’t have the physical Sim tray anymore, but
-            couldn’t have been easier! I bought an Unlocked black iPhone 14 Pro
-            Max in excellent condition and everything is PERFECT. It was super
-            easy to set up and the phone works and looks great. It truly was in
-            excellent condition. Highly recommend!!!🖤
-          </p>
         </div>
-      </div>
-      <div className="flex p-4 gap-6 max-sm:gap-3 bg-gray-100 rounded-xl">
-        <div className="flex justify-center items-start max-w-[70px] max-sm:max-w-[50px]">
-          <img src="/review.jpg" alt="" className="rounded-full" />
-        </div>
-        <div className="w-full flex flex-col gap-3 max-sm:gap-2">
-          <div>
-            <div className="flex justify-between items-center">
-              <h1 className="font-semibold font-sm">James Harris</h1>
-              <span className="text-gray-500/45 font-semibold text-xs">
-                24 January,2023
-              </span>
-            </div>
-          </div>
-          <div className="flex gap-1">
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-          </div>
-          <p className="text-sm text-gray-500  max-sm:line-clamp-4">
-            I was a bit nervous to be buying a secondhand phone from Amazon, but
-            I couldn’t be happier with my purchase!! I have a pre-paid data plan
-            so I was worried that this phone wouldn’t connect with my data plan,
-            since the new phones don’t have the physical Sim tray anymore, but
-            couldn’t have been easier! I bought an Unlocked black iPhone 14 Pro
-            Max in excellent condition and everything is PERFECT. It was super
-            easy to set up and the phone works and looks great. It truly was in
-            excellent condition. Highly recommend!!!🖤
-          </p>
-        </div>
-      </div>
-      <div className="flex p-4 gap-6 max-sm:gap-3 bg-gray-100 rounded-xl">
-        <div className="flex justify-center items-start max-w-[70px] max-sm:max-w-[50px]">
-          <img src="/review.jpg" alt="" className="rounded-full" />
-        </div>
-        <div className="w-full flex flex-col gap-3 max-sm:gap-2">
-          <div>
-            <div className="flex justify-between items-center">
-              <h1 className="font-semibold font-sm">James Harris</h1>
-              <span className="text-gray-500/45 font-semibold text-xs">
-                24 January,2023
-              </span>
-            </div>
-          </div>
-          <div className="flex gap-1">
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-            <FaStar className="w-5 h-5 max-sm:w-3 max-sm:h-3 text-yellow-400/80" />
-          </div>
-          <p className="text-sm text-gray-500  max-sm:line-clamp-4">
-            I was a bit nervous to be buying a secondhand phone from Amazon, but
-            I couldn’t be happier with my purchase!! I have a pre-paid data plan
-            so I was worried that this phone wouldn’t connect with my data plan,
-            since the new phones don’t have the physical Sim tray anymore, but
-            couldn’t have been easier! I bought an Unlocked black iPhone 14 Pro
-            Max in excellent condition and everything is PERFECT. It was super
-            easy to set up and the phone works and looks great. It truly was in
-            excellent condition. Highly recommend!!!🖤
-          </p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
