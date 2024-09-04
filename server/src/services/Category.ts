8c2450397ed9acb455863bb437aa69b4ea1c97a6
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-09-04 23:45:21 +0500

    some change

--- server/src/services/Category.ts
+++ server/src/services/Category.ts
@@ -4,7 +4,7 @@ export async function All() {
   return await prismm.category.findMany();
 }
 export async function One(name: string) {
-  return await prismm.category.findUnique({ where: { name } });
+  return await prismm.category.findFirst({ where: { name: name } });
 }
 export async function Create(name: string) {
   return await prismm.category.create({ data: { name } });
