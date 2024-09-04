fee9fce575346032f4a0e29ec55a28da88c5046d
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-09-04 23:42:18 +0500

    removed activedata

--- client/src/components/home/TopProductsButton.tsx
+++ client/src/components/home/TopProductsButton.tsx
@@ -1,20 +1,6 @@
 import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 
-const activeData = [
-  {
-    name: "new",
-    title: "New Arrivals",
-  },
-  {
-    name: "best",
-    title: "Bestsellers",
-  },
-  {
-    name: "featured",
-    title: "Featured",
-  },
-];
 type Data = {
   name: string;
   title: string;
@@ -26,29 +12,43 @@ function TopProductsButton({
   active: string;
   setActive: Function;
 }) {
-  const [data, setData] = useState<Data[]>([]);
+  const [data, setData] = useState<Data[]>();
   useEffect(() => {
-    setData(activeData);
+    setData([
+      {
+        name: "new",
+        title: "New Arrivals",
+      },
+      {
+        name: "best",
+        title: "Bestsellers",
+      },
+      {
+        name: "price",
+        title: "Price",
+      },
+    ]);
   }, []);
-  const elementCreate: JSX.Element[] = data?.map((item, i) => {
-    return (
-      <Button
-        variant={"ghost"}
-        name={item.name}
-        className="max-sm:text-xs max-sm:p-2"
-        key={i}
-        onClick={() => setActive(item.name)}
-      >
-        <span
-          className={`transition-all duration-300 ${
-            active === item.name ? "underline underline-offset-8" : ""
-          }`}
+  const elementCreate: JSX.Element[] =
+    data?.map((item, i) => {
+      return (
+        <Button
+          variant={"ghost"}
+          name={item.name}
+          className="max-sm:text-xs max-sm:p-2"
+          key={i}
+          onClick={() => setActive(item.name)}
         >
-          {item.title}
-        </span>
-      </Button>
-    );
-  });
+          <span
+            className={`transition-all duration-300 ${
+              active === item.name ? "underline underline-offset-8" : ""
+            }`}
+          >
+            {item.title}
+          </span>
+        </Button>
+      );
+    }) ?? [];
   return <div className="flex items-center justify-start">{elementCreate}</div>;
 }
 
