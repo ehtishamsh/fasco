e05ac0d489846267090bd78f784712531842db6d
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-09-07 23:53:54 +0500

    some changes

--- client/src/components/home/ShopGrid.tsx
+++ client/src/components/home/ShopGrid.tsx
@@ -13,15 +13,15 @@ function ShopGrid() {
                 <img src="/ps.png" alt="" className="max-w-full " />
               </div>
               <div className="p-4 max-sm:p-2 flex justify-center items-start flex-col">
-                <h1 className="text-5xl max-lg:text-4xl max-md:text-3xl max-sm:text-xl font-semibold mb-2 text-black">
+                <div className="text-5xl max-lg:text-4xl max-md:text-3xl max-sm:text-xl font-semibold mb-2 text-black">
                   <Reveal delayTime={0.7}>Playstation 5</Reveal>
-                </h1>
-                <p className="text-sm text-left px-2 max-sm:px-1 max-sm:text-xs text-gray-400">
+                </div>
+                <div className="text-sm text-left px-2 max-sm:px-1 max-sm:text-xs text-gray-400">
                   <Reveal delayTime={0.9}>
                     Incredibly powerful CPUs, GPUs, and an SSD with integrated
                     I/O will redefine your PlayStation experience.
                   </Reveal>
-                </p>
+                </div>
               </div>
             </Link>
 
@@ -34,18 +34,18 @@ function ShopGrid() {
                   <img src="/air.png" alt="" className="max-w-full" />
                 </div>
                 <div className="p-4 max-sm:p-2 flex justify-center items-start flex-col col-span-4">
-                  <h1 className="text-3xl max-md:text-xl max-sm:text-base mb-2 text-left px-2 max-sm:px-1">
+                  <div className="text-3xl max-md:text-xl max-sm:text-base mb-2 text-left px-2 max-sm:px-1">
                     <Reveal delayTime={0.7}>
                       Apple <br /> AirPods
                     </Reveal>
 
                     <span className="font-semibold"> Max</span>
-                  </h1>
-                  <p className="text-sm text-left px-2 max-sm:px-1 text-gray-400 max-sm:text-xs">
+                  </div>
+                  <div className="text-sm text-left px-2 max-sm:px-1 text-gray-400 max-sm:text-xs">
                     <Reveal delayTime={0.9}>
                       Computational audio. Listen, it's powerful.
                     </Reveal>
-                  </p>
+                  </div>
                 </div>
               </Link>
 
@@ -57,16 +57,16 @@ function ShopGrid() {
                   <img src="/airpods.png" alt="" className="max-w-full" />
                 </div>
                 <div className="p-4 max-sm:p-2 flex justify-center items-start flex-col col-span-4">
-                  <h1 className="text-3xl max-md:text-xl max-sm:text-base mb-2 text-left px-2 max-sm:px-1 text-white">
+                  <div className="text-3xl max-md:text-xl max-sm:text-base mb-2 text-left px-2 max-sm:px-1 text-white">
                     <Reveal delayTime={0.7}>
                       Apple <br /> AirPods
                     </Reveal>
-                  </h1>
-                  <p className="text-sm text-left px-2 max-sm:px-1 text-gray-400 max-sm:text-xs">
+                  </div>
+                  <div className="text-sm text-left px-2 max-sm:px-1 text-gray-400 max-sm:text-xs">
                     <Reveal delayTime={0.9}>
                       Computational audio. Listen, it's powerful & incredibly.
                     </Reveal>
-                  </p>
+                  </div>
                 </div>
               </Link>
             </div>
@@ -77,17 +77,17 @@ function ShopGrid() {
               to={"/laptops/apple/macbook-air-2023"}
               className="col-span-2 flex justify-center items-center flex-col"
             >
-              <h1 className="text-6xl max-lg:text-5xl max-md:text-3xl max-sm:text-2xl mb-2 text-left px-2 max-sm:px-1 text-black">
+              <div className="text-6xl max-lg:text-5xl max-md:text-3xl max-sm:text-2xl mb-2 text-left px-2 max-sm:px-1 text-black">
                 <Reveal delayTime={0.7} width="fit-content">
                   MacBook <span className="font-semibold">Air</span>
                 </Reveal>
-              </h1>
-              <p className="text-sm text-left px-2 max-sm:px-1 text-gray-400 max-sm:text-xs max-w-[400px] max-sm:max-w-[250px]">
+              </div>
+              <div className="text-sm text-left px-2 max-sm:px-1 text-gray-400 max-sm:text-xs max-w-[400px] max-sm:max-w-[250px]">
                 <Reveal delayTime={0.9} width="fit-content">
                   The new 15‑inch MacBook Air makes room for more of what you
                   love with a spacious Liquid Retina display.
                 </Reveal>
-              </p>
+              </div>
               <Reveal delayTime={1}>
                 <Button
                   className="mt-5 text-xl !p-6 max-sm:text-base max-sm:!p-4"
