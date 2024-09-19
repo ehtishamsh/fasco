208fd1af35d15d21bbc68198629a7c00c74ea230
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-09-19 23:30:47 +0500

    added sidebar to layout

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -2,11 +2,16 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "./components/ui/toaster";
+import { useState } from "react";
+import Sidebar from "./components/Sidebar";
 
 function Layout() {
+  //@ts-ignore
+  const [hide, setHide] = useState(false);
   return (
     <>
-      <Navbar />
+      <Navbar sethide={setHide} />
+      <Sidebar hide={hide} sethide={setHide} />
       <div className="mt-28 max-sm:mt-24">
         <Outlet />
       </div>
