a35e5429786cd1f4b6705abc6450a8b8915a6978
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-09-19 23:32:45 +0500

    fixed marquee

--- client/src/components/home/Brands.tsx
+++ client/src/components/home/Brands.tsx
@@ -27,39 +27,39 @@ function Brands() {
     <Reveal width="100%" delayTime={0.8}>
       <div className="py-16 my-12 max-md:py-10 max-md:my-8 max-sm:py-3 max-sm:my-3">
         <div className="marquee">
-          <ul className="marquee-content" ref={marqueeContentRef}>
+          <ul className="marquee-content max-sm:gap-16" ref={marqueeContentRef}>
             <li>
               <img
                 src="/brand-1.png"
-                className="max-w-52 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
+                className="max-w-52 max-md:max-w-28 max-sm:w-28 transition-all duration-300"
                 alt=""
               />
             </li>
             <li>
               <img
                 src="/brand-2.png"
-                className="max-w-52 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
+                className="max-w-52 max-md:max-w-28 max-sm:w-28 transition-all duration-300"
                 alt=""
               />
             </li>
             <li>
               <img
                 src="/brand-3.png"
-                className="max-w-52 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
+                className="max-w-52 max-md:max-w-28 max-sm:w-28 transition-all duration-300"
                 alt=""
               />
             </li>
             <li>
               <img
                 src="/brand-4.png"
-                className="max-w-52 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
+                className="max-w-52 max-md:max-w-28 max-sm:w-28 transition-all duration-300"
                 alt=""
               />
             </li>
             <li>
               <img
                 src="/brand-5.png"
-                className="max-w-52 max-md:max-w-28 max-sm:max-w-14 transition-all duration-300"
+                className="max-w-52 max-md:max-w-28 max-sm:w-28 transition-all duration-300"
                 alt=""
               />
             </li>
