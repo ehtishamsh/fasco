6a73a1258f8f03364ed59e41005ac156690002c3
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-09-21 23:34:25 +0500

    new color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        yellowColor: "#ffed00",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
