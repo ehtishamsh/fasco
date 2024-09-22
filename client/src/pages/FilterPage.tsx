3bc5c277b6eeee3c478b97b31408b62050a37cef
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-09-22 17:23:17 +0500

    check for category if not exist then redirect

--- client/src/pages/FilterPage.tsx
+++ client/src/pages/FilterPage.tsx
@@ -1,9 +1,35 @@
 import { BreadCrum } from "@/components/BreadCrum";
 import Filter from "@/components/filter/Filter";
-import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 function FilterPage() {
   const path = useParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const check = async () => {
+      try {
+        const res = await fetch(`http://localhost:4000/api/categories`);
+        const data = await res.json();
+        console.log(data);
+        // Redirect to 404 if category not found
+        if (data.categories.length === 0) {
+          navigate("/404");
+        }
+
+        const findCate = data.categories.find(
+          (item: any) => item.name.toLowerCase() === path.category
+        );
+        if (findCate === undefined) {
+          navigate("/404");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    check();
+    return () => {};
+  }, [path.category]);
 
   return (
     <div className="px-4">
