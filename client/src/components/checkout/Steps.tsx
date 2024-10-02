7f3e624266734a3df504e8f3d054f2113993e988
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-10-03 01:52:10 +0500

    responsive

--- client/src/components/checkout/Steps.tsx
+++ client/src/components/checkout/Steps.tsx
@@ -22,7 +22,7 @@ function Steps({
   }, [stepData]);
 
   return (
-    <div className="relative mt-6 max-sm:mt-4 mb-24 max-sm:mb-12">
+    <div className="relative mt-6 max-sm:pt-4 mb-24 max-sm:mb-12">
       {/* Progress Bar */}
       <div className="absolute -bottom-4 left-0 right-0 h-1 bg-gray-300 z-0">
         <div
@@ -35,13 +35,13 @@ function Steps({
       <div className="relative z-10 flex justify-between">
         <div className="flex justify-center items-center">
           <span
-            className={`h-10 w-10 max-sm:w-8 max-sm:h-8 ${
+            className={`h-10 w-10 max-sm:w-6 max-sm:h-6 ${
               currentStep >= 1 ? "bg-yellow-500" : "bg-yellow-200"
             } rounded-full flex justify-center items-center transition-all duration-300`}
           >
             <FaMapMarkerAlt
-              className={`text-2xl max-sm:text-lg ${
-                currentStep >= 1 ? "text-background" : "text-gray-400"
+              className={`text-2xl max-sm:text-sm ${
+                currentStep >= 1 ? "text-background" : "text-yellow-600"
               }`}
             />
           </span>
@@ -51,19 +51,19 @@ function Steps({
             }`}
           >
             <p className="text-sm max-sm:text-xs">Step 1</p>
-            <p className="text-base max-sm:text-sm font-semibold">Address</p>
+            <p className="text-base max-sm:text-xs font-semibold">Address</p>
           </div>
         </div>
 
         <div className="flex justify-center items-center">
           <span
-            className={`h-10 w-10 max-sm:w-8 max-sm:h-8 ${
+            className={`h-10 w-10 max-sm:w-6 max-sm:h-6 ${
               currentStep >= 2 ? "bg-yellow-500" : "bg-yellow-200"
             } rounded-full flex justify-center items-center transition-all duration-300`}
           >
             <FaShippingFast
-              className={`text-2xl max-sm:text-lg ${
-                currentStep >= 2 ? "text-background" : "text-gray-400"
+              className={`text-2xl max-sm:text-sm ${
+                currentStep >= 2 ? "text-background" : "text-yellow-600"
               }`}
             />
           </span>
@@ -73,19 +73,19 @@ function Steps({
             }`}
           >
             <p className="text-sm max-sm:text-xs">Step 2</p>
-            <p className="text-base max-sm:text-sm font-semibold">Shipping</p>
+            <p className="text-base max-sm:text-xs font-semibold">Shipping</p>
           </div>
         </div>
 
         <div className="flex justify-center items-center">
           <span
-            className={`h-10 w-10 max-sm:w-8 max-sm:h-8 ${
+            className={`h-10 w-10 max-sm:w-6 max-sm:h-6 ${
               currentStep >= 3 ? "bg-yellow-500" : "bg-yellow-200"
             } rounded-full flex justify-center items-center transition-all duration-300`}
           >
             <FaCreditCard
-              className={`text-2xl max-sm:text-lg ${
-                currentStep >= 3 ? "text-background" : "text-gray-400"
+              className={`text-2xl max-sm:text-sm ${
+                currentStep >= 3 ? "text-background" : "text-yellow-600"
               }`}
             />
           </span>
@@ -95,7 +95,7 @@ function Steps({
             }`}
           >
             <p className="text-sm max-sm:text-xs">Step 3</p>
-            <p className="text-base max-sm:text-sm font-semibold">Payment</p>
+            <p className="text-base max-sm:text-xs font-semibold">Payment</p>
           </div>
         </div>
       </div>
