8e4f24929e7d24519cf80441e4ffcdab8fc11806
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-10-03 01:51:09 +0500

    added back functionality

--- client/src/components/checkout/ButtonNextBack.tsx
+++ client/src/components/checkout/ButtonNextBack.tsx
@@ -1,15 +1,16 @@
 import { Button } from "../ui/button";
-import { useNavigate } from "react-router-dom";
 
-function ButtonNextBack({ handleNext }: { handleNext: () => void }) {
-  const navigate = useNavigate();
+function ButtonNextBack({
+  handleNext,
+  handleBack,
+}: { handleNext: () => void } & { handleBack: () => void }) {
   return (
     <div className="flex justify-end items-center  py-10">
       <div className="w-[500px] flex items-center gap-5">
         <Button
           variant={"outline"}
           size={"lg"}
-          onClick={() => navigate(-1)}
+          onClick={handleBack}
           className="py-6 w-full"
         >
           Back
